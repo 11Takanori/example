ea158ca53948527f1a3a4c0267c22ade849afe9a
Takanori Ishibashi <takanori.1112@gmail.com>
2016-10-19 07:56:54 +0900

    add while

--- src/main.rs
+++ src/main.rs
@@ -24,4 +24,16 @@ fn main() {
 
     println!("y is {}", y);
 
+    let mut x = 5; 
+    let mut done = false; 
+
+    while !done {
+        x += x - 3;
+
+        println!("{}", x);
+
+        if x % 5 == 0 {
+            done = true;
+        }
+    }
 }
