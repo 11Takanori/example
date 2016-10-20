b90dd6813775591a4797283e6755e8b54a19b352
Takanori Ishibashi <takanori.1112@gmail.com>
2016-10-21 07:21:41 +0900

    add Inches

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,17 @@ struct Point {
     y: Cell<i32>,
 }
 
+struct Inches(i32);
+
 fn main() {
     let point = Point { x: 5, y: Cell::new(6) };
 
     point.y.set(7);
 
     println!("y : {:?}", point.y);  
+
+    let length = Inches(10);
+
+    let Inches(integer_lenght) = length;
+    println!("length is {}f inches", integer_lenght);
 }
