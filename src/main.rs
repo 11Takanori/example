390d295bcbcae5978293096b81cb7c8b6e1fc1cc
Takanori Ishibashi <takanori.1112@gmail.com>
2016-10-22 08:44:37 +0900

    add or

--- src/main.rs
+++ src/main.rs
@@ -27,8 +27,7 @@ fn main() {
         1 => println!("one"),
         2 => println!("two"),
         3 => println!("three"),
-        4 => println!("four"),
-        5 => println!("five"),
+        4 | 5 => println!("four or five"),
         _ => println!("something else"),
     }
 }
