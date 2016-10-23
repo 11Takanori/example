adc10a722780f582223f1a671677dc05f0639d13
Takanori Ishibashi <takanori.1112@gmail.com>
2016-10-23 10:13:40 +0900

    Start new lines

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,10 @@ fn foo<T: Clone, K: Clone + Debug>(x: T, y: K) {
     println!("{:?}", y);
 }
 
-fn bar<T, K>(x: T, y: K) where T: Clone, K: Clone + Debug {
+fn bar<T, K>(x: T, y: K)
+    where T: Clone,
+          K: Clone + Debug {
+    
     x.clone();
     y.clone();
     println!("{:?}", y);
