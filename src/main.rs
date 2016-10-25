ebbc3990a1c644a76a03768c03178585dab0c8be
Takanori Ishibashi <takanori.1112@gmail.com>
2016-10-26 05:58:03 +0900

    Add borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
 fn main() {
-    let a: i32 = 42;
-    let b: i32 = a;
+    let mut a: String = "abc".to_string();
+    {
+        let b: &mut String = &mut a;
+        b.push_str("xyz");
 
-    println!("a={}, b={}", a, b);
+        println!("b={}", b);
+    }
+    println!("a={}", a);
+
+    {
+        let b: &mut String = &mut a;
+        let c: &mut String = b;
+
+        println!("c={}", c);
+    }
+
+    {
+        let b: &String = &a;
+        println!("a={}, b={}", a, b);
+
+        let c: &String = &a;
+        println!("a={}, b={}, c={}", a, b, c);
+    }
 }
