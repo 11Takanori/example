0470074efe12ca9f1c5525a35ea6f238cf387ec6
Takanori Ishibashi <takanori.1112@gmail.com>
2016-10-27 06:09:19 +0900

    Add Universal Function Call Syntax

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-fn change_truth(x: bool) -> bool {
-    !x
-}
-
 fn main() {
-    let a = true;
+    trait Foo {
+         fn f(&self);
+    }
 
-    let _y = change_truth(a);
-    println!("{}", a);
-    println!("{}", _y);
+    trait Bar {
+         fn f(&self);
+    }
+
+    struct Baz;
+
+    impl Foo for Baz {
+        fn f(&self) { println!("Baz's impl of Foo");}
+    }
+
+    impl Bar for Baz {
+        fn f(&self) { println!("Baz's impl of Bar");}
+    }
+
+    let b = Baz;
+    Foo::f(&b);
+    Bar::f(&b);
 }
