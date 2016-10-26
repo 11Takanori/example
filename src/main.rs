f5a202b14ad0a7287f95137898440ff6c8dafcf1
Takanori Ishibashi <takanori.1112@gmail.com>
2016-10-27 07:37:24 +0900

    Add impl

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 #[derive(Debug)]
 struct Foo<'a> {
-    x: &'a i32
+    x: &'a i32,
+}
+
+impl <'a> Foo<'a> {
+    fn x(&self) -> &'a i32 { self.x }
 }
 
 fn main() {
     let y = &5;
     let f = Foo { x: y };
 
-    println!("{}", f.x);
+    println!("x is: {}", f.x());
 }
