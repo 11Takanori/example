3f281e8dffc2dde772956f4667c28b4d2b444dc5
Takanori Ishibashi <takanori.1112@gmail.com>
2016-10-30 06:24:40 +0900

    Add transmute

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate introduction;
 
 use introduction::english::*;
 use introduction::japanese;
+use std::mem;
 
 fn main() {
     println!("Hello in English: {}", greetings::hello());
@@ -9,4 +10,11 @@ fn main() {
 
     println!("Hello in Japanese: {}", japanese::hello());
     println!("Goodbye in Japanese: {}", japanese::goodbye());
+
+    unsafe {
+        let a = [0u8, 0u8, 0u8, 0u8];
+        let b = mem::transmute::<[u8; 4], u32>(a);
+
+        println!("{}", b);
+    }
 }
