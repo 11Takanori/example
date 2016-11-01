b172d64ec93bb1810d87ce01486d0aa80ab77546
Takanori Ishibashi <takanori.1112@gmail.com>
2016-11-02 06:10:06 +0900

    Add ownership

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,39 @@ fn main() {
 
         println!("{}", b);
     }
+
+    let (adjective, name) = two_words();
+    let name = format!("{} {}", adjective, name);
+    print_out(name);
+}
+
+fn two_words() -> (String, String) {
+    let fello = format!("fello");
+    let rustaceans = format!("restaceans");
+
+    (fello, rustaceans)
+}
+
+fn remove_vowels(name: String) -> String {
+    let mut output = String::new();
+    for c in name.chars() {
+        match c {
+            'a' | 'e' | 'i' | 'o' | 'u' => {
+            }
+            _ => {
+                output.push(c);
+            }
+        }
+    }
+    output
+}
+
+fn print_out(name: String) {
+    let devowelized_name = remove_vowels(name.clone());
+    println!("Removing vowels yields {:?}", devowelized_name);
+
+    println!("Removing vowels from {:?} yields {:?}",
+            name, devowelized_name);
 }
 
 trait Graph {
