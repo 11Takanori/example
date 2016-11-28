cd2aa162d6292f661f3211f7e07256c852691560
Takanori Ishibashi <takanori.1112@gmail.com>
2016-11-29 07:34:42 +0900

    Complete 1.2

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ fn main() {
     #[derive(Debug)]
     struct Structure(i32);
 
+    let pi = format!("{:.*}", 3, 22 as f32 /7 as f32);
+
     println!("This struct `{:?}` won't print...", Structure(3));
     println!("My name is {0}, {1} {0}", "James","Bond");
+    println!("Pi is roughly {:?}", pi);
 }
