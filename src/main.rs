a63d1cdeaac812ff79a5080f84942bbe113281c2
Takanori Ishibashi <takanori.1112@gmail.com>
2016-12-01 07:32:29 +0900

    Add fmt::Display for List

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,9 @@ struct Complex {
     imag: f64,
 }
 
+#[derive(Debug)]
+struct List(Vec<i32>);
+
 impl fmt::Display for Point {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "x: {}, y: {}", self.x, self.y)
@@ -28,13 +31,32 @@ impl fmt::Display for Complex {
     }
 }
 
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let List(ref vec) = *self;
+
+        try!(write!(f, "["));
+
+        for (count, v) in vec.iter().enumerate() {
+            if count != 0 { try!(write!(f, ", ")); }
+            try!(write!(f, "{}", v));
+        }
+
+        write!(f, "]")
+    }
+}
+
 fn main() {
-    let point = Point{ x: 1.2, y: 3.4 };
-    let complex = Complex { real: 3.3, imag: 7.2 };
+    let p = Point{ x: 1.2, y: 3.4 };
+    let c = Complex { real: 3.3, imag: 7.2 };
+    let v = List(vec![1, 2, 3]);
 
-    println!("Display: {}", point);
-    println!("Debug: {:?}", point);
+    println!("Display: {}", p);
+    println!("Debug: {:?}", p);
 
-    println!("Display: {}", complex);
-    println!("Debug: {:?}", complex);
+    println!("Display: {}", c);
+    println!("Debug: {:?}", c);
+
+    println!("Display: {}", v);
+    println!("Debug: {:?}", v);
 }
