c300f2776f633845a211a03576ea44e8cf01a83b
Takanori Ishibashi <takanori.1112@gmail.com>
2016-12-10 13:21:33 +0900

    Add closures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+fn apply<F>(f: F) where
+    F: FnOnce() {
+    f()
+}
+
+fn apply_to_3<F>(f: F) -> i32 where
+    F: Fn(i32) -> i32 {
+    f(3)
+}
+
 fn main() {
-    println!("Hello World!");
+    use std::mem;
+
+    let greeting = "hell";
+    let mut farewell = "goodbye".to_owned();
+    let diary = || {
+        println!("I said {}.", greeting);
+
+        farewell.push_str("!!!");
+        println!("Then I screamed {}.", farewell);
+        println!("Now I can sleep. zzzz");
+
+        mem::drop(farewell);
+    };
+
+    apply(diary);
+
+    let double = |x| 2 * x;
+
+    println!("3 doubled: {}", apply_to_3(double));
 }
