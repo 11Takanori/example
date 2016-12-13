7f6e0a6a8ed18a81a82cd40864191c39f1cae9c7
Takanori Ishibashi <takanori.1112@gmail.com>
2016-12-13 21:27:21 +0900

    Writing to CSV

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,17 @@
+extern crate csv;
+
 fn main() {
+    use csv::Writer;
+
+    let people = vec![
+        ("Yamada", "Taro", 1989),
+        ("Sato", "Takashi", 1990),
+        ("Suzuki", "Emi", 1991),
+    ];
+    let path = "sample.csv";
+    let mut writer = Writer::from_file(path).unwrap();
+
+    for row in people {
+        writer.encode(row).expect("CSV writer error");
+    }
 }
