43ba0b859e3904fa9cb516b0882012e23619f852
Takanori Ishibashi <takanori.1112@gmail.com>
2016-12-13 22:09:58 +0900

    CSV parsing

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
 extern crate csv;
 
 fn main() {
-    use csv::Writer;
+    use csv::{Writer, Reader};
 
-    let people = vec![
-        ("Yamada", "Taro", 1989),
-        ("Sato", "Takashi", 1990),
-        ("Suzuki", "Emi", 1991),
-    ];
-    let path = "sample.csv";
-    let mut writer = Writer::from_file(path).unwrap();
+    // let people = vec![
+    //     ("Yamada", "Taro", 1989),
+    //     ("Sato", "Takashi", 1990),
+    //     ("Suzuki", "Emi", 1991),
+    // ];
+    // let path = "sample.csv";
+    // let mut writer = Writer::from_file(path).unwrap();
 
-    for row in people {
-        writer.encode(row).expect("CSV writer error");
+    // for row in people {
+    //     writer.encode(row).expect("CSV writer error");
+    // }
+
+    let mut reader = Reader::from_file("sample.csv").unwrap().has_headers(false);
+    for row in reader.decode() {
+        let row: (String, String, usize) = row.unwrap();
+        println!("{:?}", row);
     }
 }
