f850b167499c42b1ec5eb7f0f811a03aca11a7c0
Takanori Ishibashi <takanori.1112@gmail.com>
2016-12-26 07:51:05 +0900

    Delete dead code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate csv;
 extern crate rustc_serialize;
 
-#[derive(RustcDecodable, RustcEncodable)]
+#[derive(RustcEncodable)]
 struct People {
     last_name: String,
     first_name: String,
@@ -9,28 +9,12 @@ struct People {
 }
 
 fn main() {
-    // use csv::{Writer,Reader};
-
-    let people = vec![
-        ("Yamada", "Taro", 1989),
-        ("Sato", "Takashi", 1990),
-        ("Suzuki", "Emi", 1991),
-    ];
-    // let path = "sample.csv";
-    let mut writer = csv::Writer::from_file("sample.csv").unwrap();
-
-    // for row in people {
-    //     writer.encode(row).expect("CSV writer error");
-    // }
-
     let people = People {
         last_name: "Takahashi".to_owned(),
         first_name: "Hanako".to_owned(),
         year: 1992,
     };
+    let mut writer = csv::Writer::from_file("sample.csv").unwrap();
 
     writer.encode(people).expect("CSV writer error");
-    writer.flush().expect("Flush error");
-
-
 }
