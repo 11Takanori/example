e38175c1d63d9f2a32f19907628c648491537991
Takanori Ishibashi <takanori.1112@gmail.com>
2017-01-03 15:13:27 +0900

    Add main4

---
+++ src/main4.rs
@@ -0,0 +1,21 @@
+extern crate csv;
+extern crate rustc_serialize;
+extern crate hyper;   
+
+#[derive(RustcEncodable)]
+struct People {
+    last_name: String,
+    first_name: String,
+    year: usize,
+}
+
+fn main() {
+    let people = People {
+        last_name: "Takahashi".to_owned(),
+        first_name: "Hanako".to_owned(),
+        year: 1992,
+    };
+    let mut writer = csv::Writer::from_file("sample.csv").unwrap();
+
+    writer.encode(people).expect("CSV writer error");
+}
