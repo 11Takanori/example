ff73e60228c900f9fa28231414b946a29d160d5b
Takanori Ishibashi <takanori.1112@gmail.com>
2017-01-06 07:45:48 +0900

    Add decode

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,50 @@ extern crate rustc_serialize;
 use rustc_serialize::Encodable;
 use rustc_serialize::json::{self, Encoder};
 
+#[derive(RustcDecodable, RustcEncodable)]
+struct Photo {
+    url: String,
+    dimensions: (u32, u32),
+}
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct User {
+    name: String,
+    post_count: u32,
+    likes_burgers: bool,
+    avatar: Option<Photo>,
+}
+
+
 fn main() {
     println!("{:?}", json::encode(&42));
+    println!("{:?}", json::encode(&vec!["to", "be", "or", "not", "to", "be"]));
+
+    let user = User {
+        name: "Zbyszek".to_string(),
+        post_count: 100u32,
+        likes_burgers: true,
+        avatar: Some(Photo {
+            url: "http://lorempixel.com/160/160/".to_string(),
+            dimensions: (160u32, 160u32),
+        }),
+    };
+    println!("{:?}", json::encode(&user));
+    let mut encoded = String::new();
+    {
+        let mut encoder = Encoder::new_pretty(&mut encoded);
+        user.encode(&mut encoder).expect("JSON encode error");
+    }
+    println!("{}", encoded);
+
+    let incoming_request = "{\"name\":\"John\",\"post_count\":2,\"likes_burgers\":false,\
+                            \"avatar\":null}";
+    let decoded: User = json::decode(incoming_request).unwrap();
+    println!("My name is {} and I {} burgers",
+             decoded.name,
+             if decoded.likes_burgers {
+                 "love"
+             } else {
+                 "don't like"
+             });
 }
