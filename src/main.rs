4abdaff428fb1901a7758d1036c680e80bb5f369
Takanori Ishibashi <takanori.1112@gmail.com>
2017-01-07 10:45:09 +0900

    Add interleave and intersperse

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ extern crate itertools;
 use itertools::Itertools;
 
 fn main() {
-    let mut words = "hello supercalifragilisticexpialidocious programmer".split(|c| c == ' ');
-    words.foreach(|word| println!("{} is {} characters long", word, word.len()));
+    // let mut words = "hello supercalifragilisticexpialidocious programmer".split(|c| c == ' ');
+    // words.foreach(|word| println!("{} is {} characters long", word, word.len()));
+
+    let even = (1..10).map(|x| x * 2);
+    let odd = (1..5).map(|x| x * 2 + 1);
+    println!("{:?}", even.interleave(odd).collect::<Vec<_>>());
+    println!("{:?}", (1..10).intersperse(15).collect::<Vec<_>>());
 }
