9bbf8a13cf1e95c6d2d6e6693006f6146e80f186
Takanori Ishibashi <takanori.1112@gmail.com>
2017-01-13 08:01:31 +0900

    sort

--- main.go
+++ main.go
@@ -1,242 +1,32 @@
-package bytes
+package sort
 
-import (
-	"errors"
-	"io"
-	"unicode/utf8"
-)
-
-type Buffer struct {
-	buf       []byte
-	off       int
-	bootstrap [64]byte
-	lastRead  readOp
+type Interface interface {
+	Len() int
+	Less(i, j int) bool
+	Swap(i, j int)
 }
 
-type readOp int
+func inserionSort(data Interface, a, b int) {
+	for i := a + 1; i < b; i++ {
 
-const (
-	opRead      readOp = -1
-	opInvalid          = 0
-	opReadRune1        = 1
-	opReadRune2        = 2
-	opReadRune3        = 3
-	opReadRune4        = 4
-)
-
-var ErrTooLarge = errors.New("bytes.Buffer: too large")
-
-func (b *Buffer) Bytes() []byte { return b.buf[b.off:] }
-
-func (b *Buffer) String() string {
-	if b == nil {
-		return "<nil>"
 	}
-	return string(b.buf[b.off:])
 }
 
-func (b *Buffer) Len() int { return len(b.buf) - b.off }
+type IntSlice []int
 
-func (b *Buffer) Cap() int { return cap(b.buf) }
+func (p IntSlice) Len() int           { return len(p) }
+func (p IntSlice) Less(i, j int) bool { return p[i] < p[j] }
+func (p IntSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
-func (b *Buffer) Truncate(n int) {
-	b.lastRead = opInvalid
-	switch {
-	case n < 0 || n > b.Len():
-		panic("bytes.Buffer: truncation out of range")
-	case n == 0:
-		b.off = 0
-	}
-	b.buf = b.buf[0 : b.off+n]
+func Sort(data Interface) {
+	n := data.Len()
+	quickSort(data, 0, n, maxDepth(n))
 }
 
-func (b *Buffer) Reset() { b.Truncate(0) }
-
-func (b *Buffer) grow(n int) int {
-	m := b.Len()
-
-	if m == 0 && b.off != 0 {
-		b.Truncate(0)
+func maxDepth(n int) int {
+	var depth int
+	for i := n; i > 0; i >>= 1 {
+		depth++
 	}
-	if len(b.buf)+n > cap(b.buf) {
-		var buf []byte
-		if b.buf == nil && n <= len(b.bootstrap) {
-			buf = b.bootstrap[0:]
-		} else if m+n <= cap(b.buf)/2 {
-			copy(b.buf[:], b.buf[b.off:])
-			buf = b.buf[:m]
-		} else {
-			buf = makeSlice(2*cap(b.buf) + n)
-			copy(buf, b.buf[b.off:])
-		}
-		b.buf = buf
-		b.off = 0
-	}
-	b.buf = b.buf[0 : b.off+m+n]
-	return b.off + m
-}
-
-func (b *Buffer) Grow(n int) {
-	if n < 0 {
-		panic("bytes.Buffer.Grow: negative cout")
-	}
-	m := b.grow(n)
-	b.buf = b.buf[0:m]
-}
-
-func (b *Buffer) WriteString(s string) (n int, err error) {
-	b.lastRead = opInvalid
-	m := b.grow(len(s))
-	return copy(b.buf[m:], s), nil
-}
-
-const MinRead = 512
-
-func (b *Buffer) ReadFrom(r io.Reader) (n int64, err error) {
-	b.lastRead = opInvalid
-
-	if b.off >= len(b.buf) {
-		b.Truncate(0)
-	}
-	for {
-		if free := cap(b.buf) - len(b.buf); free < MinRead {
-			newBuf := b.buf
-			if b.off+free < MinRead {
-				newBuf = makeSlice(2*cap(b.buf) + MinRead)
-			}
-			copy(newBuf, b.buf[b.off:])
-			b.buf = newBuf[:len(b.buf)-b.off]
-			b.off = 0
-		}
-		m, e := r.Read(b.buf[len(b.buf):cap(b.buf)])
-		b.buf = b.buf[0 : len(b.buf)+m]
-		n += int64(m)
-		if e == io.EOF {
-			break
-		}
-		if e != nil {
-			return n, e
-		}
-	}
-	return n, nil
-}
-
-func makeSlice(n int) []byte {
-	defer func() {
-		if recover() != nil {
-			panic(ErrTooLarge)
-		}
-	}()
-	return make([]byte, n)
-}
-
-func (b *Buffer) WriteTo(w io.Writer) (n int64, err error) {
-	b.lastRead = opInvalid
-	if b.off < len(b.buf) {
-		nBytes := b.Len()
-		m, e := w.Write(b.buf[b.off:])
-		if m > nBytes {
-			panic("bytes.Buffer.WriteTo: invalid Write count")
-		}
-		b.off += m
-		n = int64(m)
-		if e != nil {
-			return n, e
-		}
-		if m != nBytes {
-			return n, io.ErrShortWrite
-		}
-	}
-	b.Truncate(0)
-	return
-}
-
-func (b *Buffer) WriteByte(c byte) error {
-	b.lastRead = opInvalid
-	m := b.grow(1)
-	b.buf[m] = c
-	return nil
-}
-
-func (b *Buffer) Read(p []byte) (n int, err error) {
-	b.lastRead = opInvalid
-	if b.off >= len(b.buf) {
-		b.Truncate(0)
-		if len(p) == 0 {
-			return
-		}
-		return 0, io.EOF
-	}
-	n = copy(p, b.buf[b.off:])
-	b.off += n
-	if n > 0 {
-		b.lastRead = opRead
-	}
-	return
-}
-
-func (b *Buffer) Next(n int) []byte {
-	b.lastRead = opInvalid
-	m := b.Len()
-	if n > m {
-		n = m
-	}
-	data := b.buf[b.off : b.off+n]
-	b.off += n
-	if n > 0 {
-		b.lastRead = opRead
-	}
-	return data
-}
-
-func (b *Buffer) ReadByte() (byte, error) {
-	b.lastRead = opInvalid
-	if b.off >= len(b.buf) {
-		b.Truncate(0)
-		return 0, io.EOF
-	}
-	c := b.buf[b.off]
-	b.off++
-	b.lastRead = opRead
-	return c, nil
-}
-
-func (b *Buffer) ReadRune() (r rune, size int, err error) {
-	b.lastRead = opInvalid
-	if b.off >= len(b.buf) {
-		b.Truncate(0)
-		return 0, 0, io.EOF
-	}
-	c := b.buf[b.off]
-	if c < utf8.RuneSelf {
-		b.off++
-		b.lastRead = opReadRune1
-		return rune(c), 1, nil
-	}
-	r, n := utf8.DecodeRune(b.buf[b.off:])
-	b.off += n
-	b.lastRead = readOp(n)
-	return r, n, nil
-}
-
-func (b *Buffer) UnreadRune() error {
-	if b.lastRead <= opInvalid {
-		return errors.New("bytes.Buffer: UnreadRune: previous operation was not ReadRune")
-	}
-	if b.off >= int(b.lastRead) {
-		b.off -= int(b.lastRead)
-	}
-	b.lastRead = opInvalid
-	return nil
-}
-
-func (b *Buffer) UnreadByte() error {
-	if b.lastRead == opInvalid {
-		return errors.New("bytes.Buffer: UnreadByte: previous operation was not a read")
-	}
-	b.lastRead = opInvalid
-	if b.off > 0 {
-		b.off--
-	}
-	return nil
+	return depth * 2
 }
