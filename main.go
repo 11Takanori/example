bd47076a3264bcab8e429dece295b74b0b90d0af
Takanori Ishibashi <takanori.1112@gmail.com>
2017-01-14 09:55:47 +0900

    Add sort

--- main.go
+++ main.go
@@ -18,6 +18,33 @@ func (p IntSlice) Len() int           { return len(p) }
 func (p IntSlice) Less(i, j int) bool { return p[i] < p[j] }
 func (p IntSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
+func quickSort(data Interface, a, b, maxDepth int) {
+	for b-a > 12 {
+		if maxDepth == 0 {
+			heapSort(data, a, b)
+			return
+		}
+		maxDepth--
+		mlo, mhi := daPivot(data, a, b)
+
+		if mlo-a < b-mhi {
+			quickSort(data, a, mlo, maxDepth)
+			a = mhi
+		} else {
+			quickSort(data, mhi, b, maxDepth)
+			b = mlo
+		}
+	}
+	if b-a > 1 {
+		for i := a + 6; i < b; i++ {
+			if data.Less(i, i-6) {
+				data.Swap(i, i-6)
+			}
+		}
+		inserionSort(data, a, b)
+	}
+}
+
 func Sort(data Interface) {
 	n := data.Len()
 	quickSort(data, 0, n, maxDepth(n))
