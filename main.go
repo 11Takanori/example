ead4d4f0370798552fad5488eb017b0781f07d77
Takanori Ishibashi <takanori.1112@gmail.com>
2017-01-14 10:36:20 +0900

    Fix sort

--- main.go
+++ main.go
@@ -8,7 +8,42 @@ type Interface interface {
 
 func inserionSort(data Interface, a, b int) {
 	for i := a + 1; i < b; i++ {
+		for j := i; j > a && data.Less(j, j-1); j-- {
+			data.Swap(j, j-1)
+		}
+	}
+}
 
+func heapSort(data Interface, a, b int) {
+	first := a
+	lo := 0
+	hi := b - a
+
+	for i := (hi - 1) / 2; i >= 0; i-- {
+		siftDown(data, i, hi, first)
+	}
+
+	for i := hi - 1; i >= 0; i-- {
+		data.Swap(first, first+i)
+		siftDown(data, lo, i, first)
+	}
+}
+
+func siftDown(data Interface, lo, hi, first int) {
+	root := lo
+	for {
+		child := 2*root + 1
+		if child >= hi {
+			break
+		}
+		if child+1 < hi && data.Less(first+child, first+child+1) {
+			child++
+		}
+		if !data.Less(first+root, first+child) {
+			return
+		}
+		data.Swap(first+root, first+child)
+		root = child
 	}
 }
 
@@ -25,7 +60,7 @@ func quickSort(data Interface, a, b, maxDepth int) {
 			return
 		}
 		maxDepth--
-		mlo, mhi := daPivot(data, a, b)
+		mlo, mhi := doPivot(data, a, b)
 
 		if mlo-a < b-mhi {
 			quickSort(data, a, mlo, maxDepth)
