556035c904415808c277cd810e7f36482e2b3183
Takanori Ishibashi <takanori.1112@gmail.com>
2017-03-12 08:31:03 +0900

    Add Initialize

--- c/hash/ClosedHash.c
+++ c/hash/ClosedHash.c
@@ -18,3 +18,16 @@ static void SetBucket(Bucket *n, const Member *x, Status stat)
     n->data = *x;
     n->stat = stat;
 }
+
+int Initialize(ClosedHash *h, int size)
+{
+    int i;
+    if ((h->table = calloc(size, sizeof(Bucket))) == NULL) {
+        h->size = 0;
+        return 0;
+    }
+    h->size = size;
+    for (i = 0; i < size; i++)
+        h->table[i].stat = Empty;
+    return 1;
+}
