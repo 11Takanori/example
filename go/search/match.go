0a17d8f90f6313c0414e2de7e5c904b10c04a69b
Takanori Ishibashi <takanori.1112@gmail.com>
2017-04-03 07:50:21 +0900

    Add kpm match

--- go/search/match.go
+++ go/search/match.go
@@ -20,6 +20,42 @@ func bf_match(txt, pat string) int {
 	return -1
 }
 
+func kmp_match(txt, pat string) int {
+	pt, pp := 1, 0
+	skip := make([]int, 1024)
+
+	for pt < len(pat)-1 {
+		if pat[pt] == pat[pp] {
+			skip[pt] = pp
+			pt++
+			pp++
+		} else if pp == 0 {
+			skip[pt] = pp
+			pt++
+		} else {
+			pp = skip[pp]
+		}
+	}
+
+	pt, pp = 0, 0
+	for pt <= len(txt)-1 && pp <= len(pat)-1 {
+		if txt[pt] == pat[pp] {
+			pt++
+			pp++
+		} else if pp == 0 {
+			pt++
+		} else {
+			pp = skip[pp]
+		}
+	}
+
+	if pp > len(pat)-1 {
+		return pt - pp
+	}
+
+	return -1
+}
+
 func main() {
 	var s1, s2 string
 	var idx int
@@ -27,10 +63,10 @@ func main() {
 	fmt.Println("text: ")
 	fmt.Scan(&s1)
 
-	fmt.Println("patternxt: ")
+	fmt.Println("pattern: ")
 	fmt.Scan(&s2)
 
-	idx = bf_match(s1, s2)
+	idx = kmp_match(s1, s2)
 
 	if idx == -1 {
 		fmt.Println("not match")
