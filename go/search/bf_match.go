6e884e07e7efb12be136c4f906217ae921748002
Takanori Ishibashi <takanori.1112@gmail.com>
2017-04-02 22:06:41 +0900

    Add bf serch

---
+++ go/search/bf_match.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func bf_match(txt, pat string) int {
+	// var pt, pp int
+	pt, pp := 0, 0
+
+	for pt <= len(txt)-1 && pp <= len(pat)-1 {
+		if txt[pt] == pat[pp] {
+			pt++
+			pp++
+		} else {
+			pt = pt - pp + 1
+			pp = 0
+		}
+	}
+	if pp == len(pat) {
+		return pt - pp
+	}
+	return -1
+}
+
+func main() {
+	var s1, s2 string
+	var idx int
+
+	fmt.Println("text: ")
+	fmt.Scan(&s1)
+
+	fmt.Println("patternxt: ")
+	fmt.Scan(&s2)
+
+	idx = bf_match(s1, s2)
+
+	if idx == -1 {
+		fmt.Println("not match")
+	} else {
+		fmt.Printf("%d\n", idx+1)
+	}
+}
