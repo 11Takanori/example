7c83d9a65eba70cfaf8dc07c20f87056648c4b7e
Takanori Ishibashi <takanori.1112@gmail.com>
2017-04-08 10:04:41 +0900

    Use lexical scoping

--- scheme/sicp/1_6.scm
+++ scheme/sicp/1_6.scm
@@ -4,12 +4,12 @@
   (/ (+ x y) 2))
 
 (define (sqrt x)
-  (define (good-enough? guess x)
+  (define (good-enough? guess)
     (< (abs (- (square guess) x)) 0.001))
-  (define (improve guess x)
+  (define (improve guess)
     (average guess (/ x guess)))
-  (define (sqrt-iter guess x)
-    (if (good-enough? guess x)
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
       guess
-      (sqrt-iter (improve guess x) x)))
-  (sqrt-iter 1.0 x))
+      (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
