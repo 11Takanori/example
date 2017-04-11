fc8ab8e3922ce3eae740d016a7f59dd6f56818cb
Takanori Ishibashi <takanori.1112@gmail.com>
2017-04-11 22:28:13 +0900

    Add iterative process

--- scheme/sicp/1_11.scm
+++ scheme/sicp/1_11.scm
@@ -1,6 +1,18 @@
-;recursive
-(define (f n)
+;recursive process
+(define (fr n)
   (cond ((< n 3) n)
-  (else (+ (f (- n 1))
-           (* (f (- n 2)) 2)
-           (* (f (- n 3)) 3)))))
+  (else (+ (fr (- n 1))
+           (* (fr (- n 2)) 2)
+           (* (fr (- n 3)) 3)))))
+
+;iterative process
+(define (fi n)
+  (define (f-iter a b c count max)
+    (cond ((> count max) a)
+          ((< max 3) n)
+          (else (f-iter (+ a (* b 2) (* c 3))
+                        a
+                        b
+                        (+ count 1)
+                        max))))
+  (f-iter 2 1 0 3 n))
