da4ceecd82d1e176a8e702aacda65c5cc7e77948
Takanori Ishibashi <takanori.1112@gmail.com>
2017-04-13 08:03:31 +0900

    Use sequential square

--- scheme/sicp/1_16.scm
+++ scheme/sicp/1_16.scm
@@ -26,3 +26,14 @@
 ;(expt-iter 2 2 2)
 ;(expt-iter 2 1 4)
 ;(expt-iter 2 0 8)
+
+;use sequential square
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+
+(define (square x) (* x x))
+
+(define (even? n)
+  (= (mod n 2) 0))
