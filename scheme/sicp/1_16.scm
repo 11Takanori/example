23f425fc837cebfe521888e2351abc8cecd2ef5d
Takanori Ishibashi <takanori.1112@gmail.com>
2017-04-13 07:48:58 +0900

    Add iterative process

--- scheme/sicp/1_16.scm
+++ scheme/sicp/1_16.scm
@@ -9,3 +9,20 @@
 ;(* 2 (* 2 (expt 2 1))
 ;(* 2 (* 2 (* 2 (expt 2 0)))
 ;(* 2 (* 2 (* 2 1)))
+
+
+;iterative process
+(define (expt b n)
+  (define (expt-iter b counter product)
+    (if (= counter 0)
+        product
+        (expt-iter b
+                   (- counter 1)
+                   (* b product))))
+  (expt-iter b n 1))
+
+;(expt 2 3)
+;(expt-iter 2 3 1)
+;(expt-iter 2 2 2)
+;(expt-iter 2 1 4)
+;(expt-iter 2 0 8)
