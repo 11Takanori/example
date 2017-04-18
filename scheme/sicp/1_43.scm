6f81ff14ebd1a5dadcf3a677b4219f0bc107f94d
Takanori Ishibashi <takanori.1112@gmail.com>
2017-04-19 07:49:44 +0900

    Add iterative process

--- scheme/sicp/1_43.scm
+++ scheme/sicp/1_43.scm
@@ -3,9 +3,18 @@
 (define (compose f g)
   (lambda (x) (f (g x))))
 
+;recursive process
 (define (repeated f n)
   (if (< n 1)
       (lambda (x) x)
       (compose f (repeated f (- n 1)))))
+
+;iterative process
+(define (repeated f n)
+  (define (iter n result)
+    (if (< n 1)
+        result
+        (iter (- n 1) (compose f result))))
+  (iter n (lambda (x) x)))
 
 ((repeated square 2) 5)
