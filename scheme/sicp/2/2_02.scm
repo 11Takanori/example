665a2b6989813fa06f52b16245cef5f457ab270c
Takanori Ishibashi <takanori.1112@gmail.com>
2017-04-21 08:09:27 +0900

    Fix 2_02

--- scheme/sicp/2/2_02.scm
+++ scheme/sicp/2/2_02.scm
@@ -1,3 +1,21 @@
+(define (make-segment p1 p2)
+  (cons p1 p2))
+
+(define (start-segment s)
+  (car s))
+
+(define (end-segment s)
+  (cdr s))
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point p)
+  (car p))
+
+(define (y-point p)
+  (cdr p))
+
 (define (print-point p)
   (newline)
   (display "(")
@@ -6,4 +24,12 @@
   (display (y-point p))
   (display ")"))
 
+(define (midpoint-segment s)
+  (define (mid-point a b)
+    (/ (+ a b) 2))
+  (make-point (mid-point (x-point (start-segment s)) (x-point (end-segment s)))
+              (mid-point (y-point (start-segment s)) (y-point (end-segment s)))))
+
+(print-point (midpoint-segment
+              (make-segment (make-point 2 4) (make-point 4 8))))
   
