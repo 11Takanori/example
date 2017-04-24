2b81b65e83fc34b971868c1eee269402a5479150
Takanori Ishibashi <takanori.1112@gmail.com>
2017-04-25 08:08:05 +0900

    Fix indent

--- scheme/sicp/2/2_20.scm
+++ scheme/sicp/2/2_20.scm
@@ -2,14 +2,14 @@
   (if (null? list1)
       list2
       (cons (car list1) (append (cdr list1) list2))))
-      
+
 (define (same-parity a . z)
   (define (iter items answer)
     (if (null? items)
         answer
         (iter (cdr items)
               (if (= (mod (car items) 2)
-                    (mod a 2))
+                     (mod a 2))
                   (append answer (list (car items)))
                   answer))))
   (iter z (list a)))
