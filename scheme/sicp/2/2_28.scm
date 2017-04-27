2f7f77f3b99c686071d5c30b708ddc168c33ee2b
Takanori Ishibashi <takanori.1112@gmail.com>
2017-04-28 07:30:03 +0900

    Add another fringe

--- scheme/sicp/2/2_28.scm
+++ scheme/sicp/2/2_28.scm
@@ -5,5 +5,11 @@
         ((pair? (car l)) (append (fringe (car l)) (fringe (car (cdr l)))))
         (else (append (list (car l)) (cdr l)))))
 
+(define (fringe l)
+  (cond ((null? l) l)
+        ((pair? l) (append (fringe (car l))
+                           (fringe (cdr l))))
+        (else (list l))))
+
 (fringe x) ; (1 2 3 4)
 (fringe (list x x)) ; (1 2 3 4 1 2 3 4)
