f3d52707c341b466b034e2b874bfbb2e3b0452c8
Takanori Ishibashi <takanori.1112@gmail.com>
2017-04-28 07:39:38 +0900

    Add another deep-reverse

--- scheme/sicp/2/2_27.scm
+++ scheme/sicp/2/2_27.scm
@@ -20,4 +20,10 @@
         (else
         (append (deep-reverse (cdr l))
                 (list (car l))))))
+
+(define (deep-reverse l)
+  (if (not (pair? l))
+      l
+      (reverse (map deep-reverse l))))
+
 (deep-reverse x)
