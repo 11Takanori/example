9689d3cf6bba0adc156abd1189c77ce3f3e2bf8a
Takanori Ishibashi <takanori.1112@gmail.com>
2017-04-29 15:04:31 +0900

    Add comment

--- scheme/sicp/2/2_33.scm
+++ scheme/sicp/2/2_33.scm
@@ -7,6 +7,13 @@
 (define (map p sequence)
   (accumulate (lambda (x y) (cons (p x) y)) () sequence))
 
+; (map square (list 1 2 3 4 5))
+; (cons (square 1) (accumulate op () (2 3 4 5))))
+; (cons (square 1) (cons (square 2) (accumulate op () (3 4 5))))
+; (cons (square 1) (cons (square 2) (cons (square 3) (accumulate op () (4 5))))
+; (cons (square 1) (cons (square 2) (cons (square 3) (cons (square 4) (accumulate op () (5))))
+; (cons (square 1) (cons (square 2) (cons (square 3) (cons (square 4) (cons (square 5) ()))
+
 (define (append seq1 seq2)
   (accumulate cons seq2 seq1))
 
