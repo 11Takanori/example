79d2fcf2b684c4fc2ee54d913049e016e4c3e257
Takanori Ishibashi <takanori.1112@gmail.com>
2017-04-29 16:00:41 +0900

    Add comment

--- scheme/sicp/2/2_34.scm
+++ scheme/sicp/2/2_34.scm
@@ -9,4 +9,10 @@
               0
               coefficient-sequence))
 
-(horner-eval 2 (list 1 3 0 5 0 1))
+;(horner-eval 2 (list 1 3 0 5 0 1))
+;(+ (* (accumulate op 0 (cdr (list 1 3 0 5 0 1))) 2) (car (list 1 3 0 5 0 1))
+;(+ (* (accumulate op 0 (3 0 5 0 1)) 2) 1)
+;(+ (* (+ (* (accumulate op 0 (0 5 0 1)) 2) 3) 2) 1)
+;(+ (* (+ (* (+ (* (accumulate op 0 (5 0 1)) 2) 0) 2) 3) 2) 1)
+;(+ (* (+ (* (+ (* (+ (* (accumulate op 0 (0 1)) 2) 5) 2) 0) 2) 3) 2) 1)
+;(+ (* (+ (* (+ (* (+ (* (+ (* 1 2) 0) 2) 5) 2) 0) 2) 3) 2) 1)
