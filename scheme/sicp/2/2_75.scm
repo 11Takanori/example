e847f6983cdac8b98c3b78376b383d07a8da9624
Takanori Ishibashi <takanori.1112@gmail.com>
2017-05-17 07:28:31 +0900

    make-from-mag-ang

--- scheme/sicp/2/2_75.scm
+++ scheme/sicp/2/2_75.scm
@@ -10,3 +10,13 @@
   dispatch)
 
 (define (apply-generic op arg) (arg op))
+
+
+(define (make-from-mag-ang mag angle)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) (* mag (cos a)))
+          ((eq? op 'imag-part) (* mag (sin a)))
+          ((eq? op 'magnitude) mag)
+          ((eq? op 'angle) angle)
+          (else (erro "Unknown op -- MAKE-FROM-MAG-ANG" op))))
+  dispatch)
