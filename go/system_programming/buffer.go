fe0fae51fecc7b00a692d19c608818ed3147f637
Takanori Ishibashi <takanori.1112@gmail.com>
2017-05-23 07:16:43 +0900

    Add buffer.WriteString

--- go/system_programming/buffer.go
+++ go/system_programming/buffer.go
@@ -8,5 +8,10 @@ import (
 func main() {
 	var buffer bytes.Buffer
 	buffer.Write([]byte("bytes.Buffer example\n"))
+
+	// same
+	// buffer.WriteString("bytes.Buffer example\n")
+
 	fmt.Println(buffer.String())
+
 }
