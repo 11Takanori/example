75a0c0a366718ecb6df809b6c0fb05a33cc14b84
Takanori Ishibashi <takanori.1112@gmail.com>
2017-05-24 20:47:25 +0900

    Fix net.go

--- go/system_programming/net.go
+++ go/system_programming/net.go
@@ -1,16 +1,22 @@
 package main
 
 import (
-	"io"
-	"net"
+	"net/http"
 	"os"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "ascii.jp:80")
+	// conn, err := net.Dial("tcp", "ascii.jp:80")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// conn.Write([]byte("GET / HTTP/1.0\r\nHost: ascii.jp\r\n\r\n"))
+	// io.Copy(os.Stdout, conn)
+
+	request, err := http.NewRequest("GET", "http://ascii.jp", nil)
 	if err != nil {
 		panic(err)
 	}
-	conn.Write([]byte("GET / HTTP/1.0\r\nHost: ascii.jp\r\n\r\n"))
-	io.Copy(os.Stdout, conn)
+	request.Header.Set("X-TEST", "ヘッダーも追加できます")
+	request.Write(os.Stdout)
 }
