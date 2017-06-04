4e8298b08f7ca29a1a3a9d5f24847a9ecd1d421a
Takanori Ishibashi <takanori.1112@gmail.com>
2017-06-04 13:18:02 +0900

    Add 3_13

---
+++ scheme/sicp/3/3_13.scm
@@ -0,0 +1,14 @@
+(define (last-pair x)
+  (if (null? (cdr x))
+      x
+      (last-pair (cdr x))))
+
+(define (make-cycle x)
+  (set-cdr! (last-pair x) x)
+  x)
+
+(define z (make-cycle (list 'a 'b 'c)))
+
+(last-pair z)
+
+; cがaを指すので無限ループ
