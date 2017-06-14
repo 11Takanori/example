bf60a07be34d5f2f2f5bc978f1d02eef4ccf25cb
Takanori Ishibashi <takanori.1112@gmail.com>
2017-06-15 08:00:46 +0900

    Add function

--- scheme/sicp/3/3_31.scm
+++ scheme/sicp/3/3_31.scm
@@ -13,6 +13,58 @@
 
 (define (segment-queue s) (cdr s))
 
+(define (make-agenda) (list 0))
+
+(define (current-time agenda) (car agenda))
+
+(define (set-current-time! agenda time)
+  (set-car! agenda time))
+
+(define (segments agenda) (cdr agenda))
+
+(define (set-segments! agenda segments)
+  (set-cdr! agenda segments))
+
+(define (first-segment agenda) (car (segments agenda)))
+
+(define (rest-segment agenda) (cdr (segments agenda)))
+
+(define (empty-agenda? agenda)
+  (null? (segments agenda)))
+
+(define (add-agenda! time action agenda)
+  (define (belong-before? segments)
+    (or (null? segments)
+        (< time (segment-time (car segments)))))
+  (define (make-new-time-segment time action)
+    (let ((q (make-queue)))
+      (insert-queue! q action)
+      (make-time-segment time q)))
+  (define (add-to-segments! segments)
+    (if (= (segment-time (car segments)) time)
+        (insert-queue! (segment-queue (car segments))
+                       action)
+        (let ((rest (cdr segments)))
+          (if (belong-before? rest)
+              (set-cdr!
+               segments
+               (cons (make-new-time-segment time action)
+                     (cdr segments)))
+               (add-to-segments! rest)))))
+   (let ((segments (segments agenda)))
+     (if (belong-before? segments)
+         (set-segments!
+           agenda
+           (cons (make-new-time-segment time action)
+                 segments))
+         (add-to-segments! segments))))
+
+(define (remove-first-agenda-item! agenda)
+  (let ((q segment-queue (first-segment agenda))))
+    (delete-queue! q)
+    (if (empty-agenda? q)
+        (set-segments! agenda (rest-segments agenda))))
+
 ;;;;;;
 
 (define (probe name wire)
