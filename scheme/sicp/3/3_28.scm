a51e8bef6d88a600a67531743912617c857ff227
Takanori Ishibashi <takanori.1112@gmail.com>
2017-06-19 08:09:29 +0900

    Fix 3_28

--- scheme/sicp/3/3_28.scm
+++ scheme/sicp/3/3_28.scm
@@ -10,12 +10,9 @@
   'ok)
 
 (define (or-gate a1 a2 output)
-  (define (or-action-procedure)
-    (let ((new-value
-           (logical-or (get-signal a1) (get-signal a2))))
-      (after-delay or-gate-delay
-                   (lambda ()
-                     (set-signal! output new-value)))))
-  (add-action! a1 or-action-procedure)
-  (add-action! a2 or-action-procedure)
-  'ok)
+  (let ((b (make-wire)) (c (make-wire)) (d (make-wire)))
+    (inverter a1 b)
+    (inverter a2 c)
+    (and-gate b c d)
+    (inverter d output)
+    'ok))
