1b831adc0304161deb5c2a9efe5fa3bd1e26b6ce
Takanori Ishibashi <takanori.1112@gmail.com>
2017-06-20 08:06:16 +0900

    Add function

--- scheme/sicp/3/3_32.scm
+++ scheme/sicp/3/3_32.scm
@@ -6,4 +6,192 @@
 (probe 'c c)
 (and-gate a b c)
 (set-signal! b 1)
-(propagate) 
+(propagate)
+
+;;;;;;;;
+(define (make-wire)
+  (let ((signal-value 0) (action-procedures '()))
+    (define (set-signal! new-value)
+      (if (not (= signal-value new-value))
+          (begin (set! signal-value new-value)
+                 (call-each action-procedures))
+          'done))
+    (define (accept-action-procedure! proc)
+      (set! action-procedures (cons proc action-procedures))
+      (proc))
+    (define (dispatch m)
+      (cond ((eq? m 'get-signal) signal-value)
+            ((eq? m 'set-signal!) set-my-signal!)
+            ((eq? m 'add-action!) accept-action-procedure!)
+            (else (error "Unknown operation -- WIRE" m))))
+  dispatch))
+
+(define (call-each procedures)
+  (if (null? procedures)
+      'done
+      (begin
+        ((car procedures))
+        (call-each (cdr procedures)))))
+
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'ok)
+
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+
+(define (logical-and s1 s2)
+  (cond ((and (= s1 1) (= s2 1)) 1)
+        ((and (= s1 1) (= s2 0)) 0)
+        ((and (= s1 0) (= s2 1)) 0)
+        ((and (= s1 0) (= s2 0)) 0)
+        (else (error "Invalid signal" s1 s2))))
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value
+            (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+(define (add-to-agenda! time action agenda)
+  (define (belong-before? segments)
+    (or (null? segments)
+        (< time (segment-time (car segments)))))
+  (define (make-new-time-segment time action)
+    (let ((q (make-queue)))
+      (insert-queue! q action)
+      (make-time-segment time q)))
+  (define (add-to-segments! segments)
+    (if (= (segment-time (car segments)) time)
+        (insert-queue! (segment-queue (car segments)) action)
+        (let ((rest (cdr segments)))
+          (if (belong-before? rest)
+              (set-cdr!
+               segments
+               (cons (make-new-time-segment time action)
+                     (cdr segments)))
+               (add-to-segments! rest)))))
+   (let ((segments (segments agenda)))
+     (if (belong-before? segments)
+         (set-segments!
+           agenda
+           (cons (make-new-time-segment time action)
+                 segments))
+         (add-to-segments! segments))))
+
+(define (after-delay delay action)
+  (add-to-agenda! (+ delay (current-time the-agenda))
+                  action
+                  the-agenda))
+
+(define (make-time-segment time queue)
+  (cons time queue))
+
+(define (segment-time s) (car s))
+
+(define (segment-queue s) (cdr s))
+
+(define (make-agenda) (list 0))
+
+(define (current-time agenda) (car agenda))
+
+(define (set-current-time! agenda time)
+  (set-car! agenda time))
+
+(define (segments agenda) (cdr agenda))
+
+(define (set-segments! agenda segments)
+  (set-cdr! agenda segments))
+
+(define (front-ptr queue) (car queue))
+
+(define (rear-ptr queue) (cdr queue))
+
+(define (set-front-ptr! queue item) (set-car! queue item))
+
+(define (set-rear-ptr! queue item) (set-cdr! queue item))
+
+(define (empty-queue? queue) (null? (front-ptr queue)))
+
+(define (make-queue) (cons '() '()))
+
+(define (current-time agenda) (car agenda))
+
+(define (segments agenda) (cdr agenda))
+
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+          (set-front-ptr! queue new-pair)
+          (set-rear-ptr! queue new-pair)
+          queue)
+        (else
+          (set-cdr! (rear-ptr queue) new-pair)
+          (set-rear-ptr! queue new-pair)
+          queue))))
+
+(define (or-gate a1 a2 output)
+  (let ((b (make-wire)) (c (make-wire)) (d (make-wire)))
+    (inverter a1 b)
+    (inverter a2 c)
+    (and-gate b c d)
+    (inverter d output)
+    'ok))
+
+(define (get-signal wire)
+  (wire 'get-signal))
+
+(define (set-signal! wire new-value)
+  ((wire 'set-signal!) new-value))
+
+(define (add-action! wire action-procedure)
+  ((wire 'add-action!) action-procedure))
+
+(define (probe name wire)
+  (add-action! wire
+               (lambda ()
+                 (newline)
+                 (display name)
+                 (display " ")
+                 (display (current-time the-agenda))
+                 (display " New-value = ")
+                 (display (get-signal wire)))))
+
+(define (make-agenda) (list 0))
+(define the-agenda (make-agenda))
+(define inverter-delay 2)
+(define and-gate-delay 3)
+(define or-gate-delay 5)
+
+(define input1 (make-wire))
+(define input2 (make-wire))
+(define output  (make-wire))
+(or-gate input1 input2 output)
+
+(probe 'input1 input1)
+(probe 'input2 input2)
+(probe 'output  output)
+
+(set-signal! input1 1)
+(propagate)
+
+(set-signal! input1 0)
+(propagate)
+
+(set-signal! input2 1)
+(propagate)
+
+(set-signal! input1 1)
+(propagate)
