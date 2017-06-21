11f92d4433475c9325a191e5294ca129f7d42a38
Takanori Ishibashi <takanori.1112@gmail.com>
2017-06-22 08:10:03 +0900

    Fix 3_32

--- scheme/sicp/3/3_32.scm
+++ scheme/sicp/3/3_32.scm
@@ -1,14 +1,52 @@
-(define the-agenda (make-agenda))
-(define and-gate-delay 3)
-(define a (make-wire))
-(define b (make-wire))
-(define c (make-wire))
-(probe 'c c)
-(and-gate a b c)
-(set-signal! b 1)
-(propagate)
+(define (get-signal wire)
+  (wire 'get-signal))
 
-;;;;;;;;
+(define (set-signal! wire new-value)
+  ((wire 'set-signal!) new-value))
+
+(define (add-action! wire action-procedure)
+  ((wire 'add-action!) action-procedure))
+
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'ok)
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value
+            (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+(define (or-gate a1 a2 output)
+  (let ((b (make-wire)) (c (make-wire)) (d (make-wire)))
+    (inverter a1 b)
+    (inverter a2 c)
+    (and-gate b c d)
+    (inverter d output)
+    'ok))
+
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+
+(define (logical-and s1 s2)
+  (cond ((and (= s1 1) (= s2 1)) 1)
+        ((and (= s1 1) (= s2 0)) 0)
+        ((and (= s1 0) (= s2 1)) 0)
+        ((and (= s1 0) (= s2 0)) 0)
+        (else (error "Invalid signal" s1 s2))))
+
 (define (make-wire)
   (let ((signal-value 0) (action-procedures '()))
     (define (set-my-signal! new-value)
@@ -33,37 +71,20 @@
         ((car procedures))
         (call-each (cdr procedures)))))
 
-(define (inverter input output)
-  (define (invert-input)
-    (let ((new-value (logical-not (get-signal input))))
-      (after-delay inverter-delay
-                   (lambda ()
-                     (set-signal! output new-value)))))
-  (add-action! input invert-input)
-  'ok)
 
-(define (logical-not s)
-  (cond ((= s 0) 1)
-        ((= s 1) 0)
-        (else (error "Invalid signal" s))))
+; agenda
+(define (first-agenda-item agenda)
+  (if (empty-agenda? agenda)
+      (error "Agenda is empty -- FIRST-AGENDA-ITEM")
+      (let ((first-seg (first-segment agenda)))
+        (set-current-time! agenda (segment-time first-seg))
+        (front-queue (segment-queue first-seg)))))
 
-(define (logical-and s1 s2)
-  (cond ((and (= s1 1) (= s2 1)) 1)
-        ((and (= s1 1) (= s2 0)) 0)
-        ((and (= s1 0) (= s2 1)) 0)
-        ((and (= s1 0) (= s2 0)) 0)
-        (else (error "Invalid signal" s1 s2))))
-
-(define (and-gate a1 a2 output)
-  (define (and-action-procedure)
-    (let ((new-value
-            (logical-and (get-signal a1) (get-signal a2))))
-      (after-delay and-gate-delay
-                   (lambda ()
-                     (set-signal! output new-value)))))
-  (add-action! a1 and-action-procedure)
-  (add-action! a2 and-action-procedure)
-  'ok)
+(define (remove-first-agenda-item! agenda)
+  (let ((q (segment-queue (first-segment agenda))))
+    (delete-queue! q)
+    (if (empty-queue? q)
+        (set-segments! agenda (rest-segments agenda)))))
 
 (define (add-to-agenda! time action agenda)
   (define (belong-before? segments)
@@ -91,6 +112,8 @@
                  segments))
          (add-to-segments! segments))))
 
+(define (current-time agenda) (car agenda))
+
 (define (after-delay delay action)
   (add-to-agenda! (+ delay (current-time the-agenda))
                   action
@@ -115,6 +138,14 @@
 (define (set-segments! agenda segments)
   (set-cdr! agenda segments))
 
+(define (first-segment agenda) (car (segments agenda)))
+
+(define (rest-segments agenda) (cdr (segments agenda)))
+
+(define (empty-agenda? agenda)
+  (null? (segments agenda)))
+
+
 (define (front-ptr queue) (car queue))
 
 (define (rear-ptr queue) (cdr queue))
@@ -127,9 +158,10 @@
 
 (define (make-queue) (cons '() '()))
 
-(define (current-time agenda) (car agenda))
-
-(define (segments agenda) (cdr agenda))
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called with an empty queue" queue)
+      (car (front-ptr queue))))
 
 (define (insert-queue! queue item)
   (let ((new-pair (cons item '())))
@@ -142,22 +174,20 @@
           (set-rear-ptr! queue new-pair)
           queue))))
 
-(define (or-gate a1 a2 output)
-  (let ((b (make-wire)) (c (make-wire)) (d (make-wire)))
-    (inverter a1 b)
-    (inverter a2 c)
-    (and-gate b c d)
-    (inverter d output)
-    'ok))
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "DELETE called with an empty queeu" queue))
+        (else
+         (set-front-ptr! queue (cdr (front-ptr queue)))
+         queue)))
 
-(define (get-signal wire)
-  (wire 'get-signal))
-
-(define (set-signal! wire new-value)
-  ((wire 'set-signal!) new-value))
-
-(define (add-action! wire action-procedure)
-  ((wire 'add-action!) action-procedure))
+(define (propagate)
+  (if (empty-agenda? the-agenda)
+      'done
+      (let ((first-itme (first-agenda-item the-agenda)))
+        (first-itme)
+        (remove-first-agenda-item! the-agenda)
+        (propagate))))
 
 (define (probe name wire)
   (add-action! wire
@@ -168,6 +198,8 @@
                  (display (current-time the-agenda))
                  (display " New-value = ")
                  (display (get-signal wire)))))
+
+
 
 (define (make-agenda) (list 0))
 (define the-agenda (make-agenda))
@@ -182,7 +214,7 @@
 
 (probe 'input1 input1)
 (probe 'input2 input2)
-(probe 'output  output)
+(probe 'output output)
 
 (set-signal! input1 1)
 (propagate)
