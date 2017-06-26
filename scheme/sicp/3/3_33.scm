3a66cbcee908e87dc6c2ace618c4256737cbf180
Takanori Ishibashi <takanori.1112@gmail.com>
2017-06-27 07:57:05 +0900

    Add for-each-except

--- scheme/sicp/3/3_33.scm
+++ scheme/sicp/3/3_33.scm
@@ -133,3 +133,11 @@
             (else (error "Unknown operation -- CONNECTOR"
                          request))))
     me))
+
+(define (for-each-except exception procedure list)
+  (define (loop items)
+    (cond ((null? items) 'done)
+          ((eq? (car items) exception) (loop (cdr items)))
+          (else (procedure (car items))
+                (loop (cdr items)))))
+  (loop list))
