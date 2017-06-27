89c4ae9e11d8adbd9cfbd90c42e9ec48b0b82450
Takanori Ishibashi <takanori.1112@gmail.com>
2017-06-28 07:20:56 +0900

    Add function

--- scheme/sicp/3/3_33.scm
+++ scheme/sicp/3/3_33.scm
@@ -141,3 +141,18 @@
           (else (procedure (car items))
                 (loop (cdr items)))))
   (loop list))
+
+(define (has-value? connector)
+  (connector 'has-value?))
+
+(define (get-value connector)
+  (connector 'value))
+
+(define (set-value! connector new-value informant)
+  ((connector 'set-value!) new-value informant))
+
+(define (forget-value! connector new-value informant)
+  ((connector 'forget) retractor))
+
+(define (connect connector new-constraint)
+  ((connector 'connect) new-constraint))
