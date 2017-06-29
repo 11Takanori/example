60b3767a38b8f1dbf9527503e85100c46731c999
Takanori Ishibashi <takanori.1112@gmail.com>
2017-06-30 07:37:46 +0900

    Add 3_34

---
+++ scheme/sicp/3/3_34.scm
@@ -0,0 +1,36 @@
+(define (squarer a b)
+  (define (process-new-value)
+    (if (has-value? b)
+        (if (< (get-value b) 0)
+            (error "squarer less than 0 -- SQUARE" (get-value b))
+            (set-value! a
+                       (sqrt (get-value b))
+                       me))
+        (set-value! b
+                    (* (get-value a) (get-value a))
+                    me)))
+  (define (process-forget-value)
+    (forget-value! a me)
+    (forget-value! b me)
+    (process-new-value))
+  (define (me request)
+    (cond ((eq? request 'I-have-a-value)
+           (process-new-value))
+          ((eq? request 'I-lost-my-value)
+           (process-forget-value))
+          (else
+            (error "Unknown request -- SQUARE" request))))
+  (connect a me)
+  (connect b me)
+me)
+
+
+(define A (make-connector))
+(define B (make-connector))
+
+(squarer A B)
+
+(probe 'a A)
+(probe 'b B)
+
+(set-value! B 169 'user)
