25fc6de4e6a6ba4c3c9aa16abc927c83dfbc93ec
Takanori Ishibashi <takanori.1112@gmail.com>
2017-07-02 22:25:13 +0900

    Add 3_38

---
+++ scheme/sicp/3/3_38.scm
@@ -0,0 +1,20 @@
+; Peter
+(set! blance (+ blance 10))
+
+; Paul
+(set! blance (- blance 20))
+
+; Mary
+(set! blance (/ blance 2))
+
+; a
+Peter, Paul, Mary, blance
+1,     2,    3,    45
+1,     3,    2,    35
+2,     1,    3,    45
+2,     3,    1,    40
+3,     1,    2,    50
+3,     2,    1,    40
+
+; b
+110, 80, 55
