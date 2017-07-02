6feed2538fa02d7b5c4b4d212feac84f73531a92
Takanori Ishibashi <takanori.1112@gmail.com>
2017-07-03 08:02:08 +0900

    Add answer

--- scheme/sicp/3/3_40.scm
+++ scheme/sicp/3/3_40.scm
@@ -8,3 +8,13 @@
 ; 10000
 ; 1000
 ; 100
+
+
+(define x 10)
+
+(define s (make-serializer))
+
+(parallel-execute (s lambda () (set! x (* x x)))
+                  (s lambda () (set! x (* x x x))))
+
+; 1000000
