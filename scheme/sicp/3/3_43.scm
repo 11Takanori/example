683eb7483d161610c89df364697271493363e30a
Takanori Ishibashi <takanori.1112@gmail.com>
2017-07-05 07:27:06 +0900

    Fix indent

--- scheme/sicp/3/3_43.scm
+++ scheme/sicp/3/3_43.scm
@@ -30,7 +30,7 @@
        account2)))
 
 (define (exchange account1 account2)
- (let ((difference (- (account1 'balance)
-                      (account2 'balance'))))
-   ((account1 'withdraw) difference)
-   ((account2 'deposit) difference)))
+  (let ((difference (- (account1 'balance)
+                       (account2 'balance'))))
+    ((account1 'withdraw) difference)
+    ((account2 'deposit) difference)))
