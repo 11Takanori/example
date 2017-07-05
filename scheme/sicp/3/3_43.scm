933e9b74b9355f8b4c7e039c869f73456d78f063
Takanori Ishibashi <takanori.1112@gmail.com>
2017-07-06 07:19:01 +0900

    Remove comma

--- scheme/sicp/3/3_43.scm
+++ scheme/sicp/3/3_43.scm
@@ -31,6 +31,6 @@
 
 (define (exchange account1 account2)
   (let ((difference (- (account1 'balance)
-                       (account2 'balance'))))
+                       (account2 'balance))))
     ((account1 'withdraw) difference)
     ((account2 'deposit) difference)))
