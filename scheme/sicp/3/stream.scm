34f5a8dfb6396dd934ccbe40e9223699841f95c9
Takanori Ishibashi <takanori.1112@gmail.com>
2017-07-15 20:06:46 +0900

    Fix stream

--- scheme/sicp/3/stream.scm
+++ scheme/sicp/3/stream.scm
@@ -1,13 +1,11 @@
+(define-syntax cons-stream
+  (syntax-rules ()
+    ((_ a b) (cons a (memo-proc (lambda () b))))))
+
 (define (stream-ref s n)
   (if (= n 0)
       (stream-car s)
       (stream-ref (stream-cdr s) (- n 1))))
-
-(define (stream-map proc s)
-  (if (stream-null? s)
-      the-empty-stream
-      (cons-stream (proc (stream-car s))
-                   (stream-map proc (stream-cdr s)))))
 
 (define (stream-for-each proc s)
   (if (stream-null? s)
@@ -26,6 +24,10 @@
 
 (define (stream-cdr stream) (force (cdr stream)))
 
+(define (stream-null? stream) (null? stream))
+
+(define the-empty-stream '())
+
 (define (stream-enumerate-interval low high)
   (if (> low high)
       the-empty-stream
@@ -33,24 +35,16 @@
        low
        (stream-enumerate-interval (+ low 1) high))))
 
-(define (stream-fillter pred stream)
+(define (stream-filter pred stream)
   (cond ((stream-null? stream) the-empty-stream)
         ((pred (stream-car stream))
          (cons-stream (stream-car stream)
-                      (stream-fillter pred
+                      (stream-filter pred
                                       (stream-cdr stream))))
-        (else (stream-fillter pred (stream-cdr stream)))))
-
-(define (cons-stream a b)
-  (cons a (delay b)))
+        (else (stream-filter pred (stream-cdr stream)))))
 
 (define (force delayed-object)
   (delayed-object))
-
-(define the-empty-stream '())
-
-(define (stream-null? a)
-  (null? a))
 
 (define (memo-proc proc)
   (let ((already-run? #f) (result #f))
@@ -61,7 +55,18 @@
                  result)
           result))))
 
-(define-syntax delay
-  (syntax-rules ()
-    ((_ <exp>)
-     (memo-proc (lambda () <exp>)))))
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
