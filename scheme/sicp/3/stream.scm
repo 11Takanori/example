e482c2a9aaa6678e92593f9f79e5168b8e78bbf2
Takanori Ishibashi <takanori.1112@gmail.com>
2017-07-17 08:50:48 +0900

    Add stream-append

--- scheme/sicp/3/stream.scm
+++ scheme/sicp/3/stream.scm
@@ -70,3 +70,9 @@
                       (stream-filter pred
                                      (stream-cdr stream))))
         (else (stream-filter pred (stream-cdr stream)))))
+
+(define (stream-append s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+                   (stream-append (stream-cdr s1) s2))))
