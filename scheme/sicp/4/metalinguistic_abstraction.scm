523980645237e67e6ad29d8b1c38b7f51f0716ca
Takanori Ishibashi <takanori.1112@gmail.com>
2017-07-21 07:52:55 +0900

    Add if

--- scheme/sicp/4/metalinguistic_abstraction.scm
+++ scheme/sicp/4/metalinguistic_abstraction.scm
@@ -104,3 +104,18 @@
 
 (define (make-lambda lambda-parameters body)
  (cons 'lambda (cons parameters body)))
+
+ ;; if
+ (define (if? exp) (tagged-list? exp 'if))
+
+ (define (if-predicate exp) (cadr exp))
+
+ (define (if-consequent exp) (caddr exp))
+
+ (define (if-alternative exp)
+   (if (not (null? (cdddr exp)))
+       (cadddr exp)
+       'false))
+
+ (define (make-if predicate consequent alternative)
+   (list 'if predicate consequent alternative))
