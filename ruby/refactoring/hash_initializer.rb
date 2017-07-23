eb08336f9a3968d5b80f32cd5a35a8429720e01b
Takanori Ishibashi <takanori.1112@gmail.com>
2017-07-23 12:27:45 +0900

    Add sample

--- ruby/refactoring/hash_initializer.rb
+++ ruby/refactoring/hash_initializer.rb
@@ -1,6 +1,7 @@
 module CustomInitializers
   def hash_initializer(*attribute_names)
     define_method(:initialize) do |args|
+
       attribute_names.each do |attribute_name|
         instance_variable_set "@#{attribute_name}", args[attribute_name]
       end
@@ -11,10 +12,11 @@ end
 Class.send :include, CustomInitializers
 
 class Hoge
-  attr_reader :foo, :bar
-  hash_initializer :foo, :bar
+  attr_reader :foo, :bar, :foo2
+  hash_initializer :foo, :bar, :foo2
 end
 
 hoge = Hoge.new(foo: 1, bar: "bar")
 p hoge.foo
 p hoge.bar
+p hoge.foo2
