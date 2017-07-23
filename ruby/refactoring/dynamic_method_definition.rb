a093bc79d760afedb742f26743873824047d4a4b
Takanori Ishibashi <takanori.1112@gmail.com>
2017-07-23 15:24:04 +0900

    Add annotation

--- ruby/refactoring/dynamic_method_definition.rb
+++ ruby/refactoring/dynamic_method_definition.rb
@@ -1,4 +1,4 @@
-class Hoge
+class Foo
   attr_accessor :state
 
   def self.def_each(*method_names, &block)
@@ -12,4 +12,18 @@ class Hoge
   def_each :failure, :error, :success do |method_name|
     self.state = method_name
   end
+end
+
+class Bar
+  attr_accessor :state
+  
+  def self.states(*args)
+    args.each do |arg|
+      define_method arg do
+        self.state = arg
+      end
+    end
+  end
+
+  states :failure, :error, :success
 end
