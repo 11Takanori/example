3aaf0b1d07ce4a532a3f5b3340923b274645a954
Takanori Ishibashi <takanori.1112@gmail.com>
2017-07-24 07:39:14 +0900

    Add cond

--- scheme/sicp/4/metalinguistic_abstraction.scm
+++ scheme/sicp/4/metalinguistic_abstraction.scm
@@ -150,3 +150,32 @@
 (define (first-operand ops) (car ops))
 
 (define (rest-operands ops) (car ops))
+
+;; cond
+(define (cond? exp) (taggle-list? exp 'cond))
+
+(define (cond-clauses exp) (cdr exp))
+
+(define (cond-else-clause? clause)
+  (eq? (cond-predicate clause) 'else))
+
+(define (cond-predicate clause) (car clause))
+
+(define (cond-actions clause) (cdr clause))
+
+(define (cond->if exp)
+  (expand-clauses (cond-clauses exp)))
+
+(define (expand-clauses clauses)
+  (if (null? clauses)
+      'false
+      (let ((first (car clauses))
+            (rest (cdr clauses)))
+        (if (cond-else-clause? first)
+            (if (null? rest)
+                (sequence->exp (cond-actions first))
+                (error "ELSE clause isn't last -- COND->IF"
+                       clause))
+            (make-if (cond-predicate first)
+                     (sequence->exp (cond-actions first))
+                     (expand-clauses rest))))))
