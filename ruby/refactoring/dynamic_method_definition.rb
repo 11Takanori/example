3f90f212cb0aa39a92b0d7d4da07b071de50be23
Takanori Ishibashi <takanori.1112@gmail.com>
2017-07-23 15:16:48 +0900

    Add DynamicMethodDefinition

---
+++ ruby/refactoring/dynamic_method_definition.rb
@@ -0,0 +1,15 @@
+class Hoge
+  attr_accessor :state
+
+  def self.def_each(*method_names, &block)
+    method_names.each do |method_name|
+      define_method method_name do
+        instance_exec method_name, &block
+      end
+    end
+  end
+
+  def_each :failure, :error, :success do |method_name|
+    self.state = method_name
+  end
+end
