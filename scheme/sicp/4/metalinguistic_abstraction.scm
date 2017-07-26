b7ca6449fde6d0063e68eadf92dff8fa697558db
Takanori Ishibashi <takanori.1112@gmail.com>
2017-07-27 07:30:50 +0900

    Add indent

--- scheme/sicp/4/metalinguistic_abstraction.scm
+++ scheme/sicp/4/metalinguistic_abstraction.scm
@@ -19,18 +19,18 @@
          (error "Unknown expression type -- EVAL" exp))))
 
 (define (apply procedure arguments)
-(cond ((primitive-procedure? procedure)
-       (apply-primitive-procedure procedure arguments))
-      ((compound-procedure? procedure)
-       (eval-sequence
-         (procedure-body procedure)
-         (extend-environment
-           (procedure-parameters procedure)
-         arguments
-         (procedure-environment procedure))))
-      (else
-       (error
-        "Unknown procedure type -- APPLY" procedure))))
+  (cond ((primitive-procedure? procedure)
+         (apply-primitive-procedure procedure arguments))
+        ((compound-procedure? procedure)
+         (eval-sequence
+           (procedure-body procedure)
+           (extend-environment
+             (procedure-parameters procedure)
+           arguments
+           (procedure-environment procedure))))
+        (else
+         (error
+          "Unknown procedure type -- APPLY" procedure))))
 
 (define (list-of-values exps env)
  (if (no-operands? exps)
