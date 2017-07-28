8e4078a90d42efd8cc1df519f7b5d8c27f12690b
Takanori Ishibashi <takanori.1112@gmail.com>
2017-07-29 08:28:29 +0900

    Add procedure

--- scheme/sicp/4/metalinguistic_abstraction.scm
+++ scheme/sicp/4/metalinguistic_abstraction.scm
@@ -187,3 +187,21 @@
             (make-if (cond-predicate first)
                      (sequence->exp (cond-actions first))
                      (expand-clauses rest))))))
+
+(define (true? x)
+  (not (eq? x #f)))
+
+(define (false? x)
+  (eq? x #f))
+
+(define (make-procedure parameters body env)
+  (list 'procedure parameters body env))
+
+(define (compound-procedure? p)
+  (tagged-list? p 'procedure))
+
+(define (procedure-parameters p) (cadr p))
+
+(define (procedure-body p) (caddr p))
+
+(define (procedure-environment p) (caddr p))
