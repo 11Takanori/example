c6daf9a0cdafd7df79de775ad8e352ecb5ade34d
Takanori Ishibashi <takanori.1112@gmail.com>
2017-09-06 07:53:22 +0900

    Add set function to HashTable

--- rust/algorism/hash_table.rs
+++ rust/algorism/hash_table.rs
@@ -1,6 +1,6 @@
 use List::*;
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 enum List {
     Cons(u32, Box<List>),
     Nil,
@@ -14,30 +14,51 @@ impl List {
     fn prepend(self, element: u32) -> List {
         Cons(element, Box::new(self))
     }
+
+    fn stringify(&self) -> String {
+        match *self {
+            Cons(head, ref tail) => {
+                format!("{}, {}", head, tail.stringify())
+            },
+            Nil => {
+                format!("Nil")
+            },
+        }
+    }
 }
+
+const  BIN_SIZE: usize = 4;
 
 #[derive(Debug)]
 struct HashTable {
-    bins: List
+    bins: Vec<List>
 }
 
 impl HashTable {
-     fn new(list: List) -> HashTable {
+     fn new() -> HashTable {
          HashTable {
-             bins: list
+             bins: vec![List::new(); BIN_SIZE]
          }
+     }
+
+     fn set(&mut self, key: u32, value: String) {
+         let index = key as usize % BIN_SIZE;
+
+         for bin in &self.bins {
+            match *bin {
+                Cons(head, ref tail) => {
+                },
+                Nil => {
+                },
+            }
+         }
+         self.bins[index] = List::new().prepend(index as u32);
      }
 }
 
 fn main() {
-    let mut list = List::new();
-
-    // list = list.prepend(1);
-    // list = list.prepend(2);
-    // list = list.prepend(3);
-    // list = list.prepend(4);
-
-    let hash_table = HashTable::new(list);
+    let mut hash_table = HashTable::new();
+    hash_table.set(6, "a".to_string());
 
     println!("{:?}", hash_table);
 
