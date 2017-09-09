e0a10e1c2d4614a4fd2183a02177b68048e61d4f
Takanori Ishibashi <takanori.1112@gmail.com>
2017-09-09 17:14:05 +0900

    Add test for iter

--- rust/algorism/lists/src/second.rs
+++ rust/algorism/lists/src/second.rs
@@ -111,4 +111,17 @@ mod test {
         assert_eq!(list.peek(), Some(&3));
         assert_eq!(list.peek_mut(), Some(&mut 3));
     }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+    }
 }
