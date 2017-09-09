386ff79e1c507990f65a78ab98793e8badce4c5a
Takanori Ishibashi <takanori.1112@gmail.com>
2017-09-10 07:41:52 +0900

    Add iter_mut

--- rust/algorism/lists/src/second.rs
+++ rust/algorism/lists/src/second.rs
@@ -15,6 +15,10 @@ pub struct Iter<'a, T: 'a> {
     next: Option<&'a Node<T>>,
 }
 
+pub struct IterMut<'a, T: 'a> {
+    next: Option<&'a mut Node<T>>,
+}
+
 impl<T> List<T> {
      pub fn new() -> Self {
          List { head: None }
@@ -56,6 +60,10 @@ impl<T> List<T> {
     pub fn iter(&self) -> Iter<T> {
         Iter { next: self.head.as_ref().map(|node| &**node) }
     }
+
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut { next: self.head.as_mut().map(|node| &mut **node) }
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -83,6 +91,17 @@ impl<'a, T> Iterator for Iter<'a, T> {
             &node.elem
         })
     }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+     type Item = &'a mut T;
+
+     fn next(&mut self) -> Option<Self::Item> {
+         self.next.take().map(|node| {
+             self.next = node.next.as_mut().map(|node| &mut **node);
+             &mut node.elem
+         })
+     }
 }
 
 
