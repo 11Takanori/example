98bedbb8766be14d6cb907e4fd2c695168f2da85
Takanori Ishibashi <takanori.1112@gmail.com>
2017-09-16 20:36:50 +0900

    Add push and pop

---
+++ rust/algorism/lists/src/fifth.rs
@@ -0,0 +1,50 @@
+pub struct List<'a, T: 'a> {
+    head: Link<T>,
+    tail: Option<&'a mut Node<T>>,
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<'a, T> List<'a, T> {
+     pub fn new() -> Self {
+         List { head: None, tail: None }
+     }
+
+     pub fn push(&'a mut self, elem: T) {
+         let new_tail = Box::new(Node {
+             elem: elem,
+             next: None,
+         });
+
+         let new_tail = match self.tail.take() {
+             Some(old_tail) => {
+                 old_tail.next = Some(new_tail);
+                 old_tail.next.as_mut().map(|node| &mut **node)
+             }
+             None => {
+                 self.head = Some(new_tail);
+                 self.head.as_mut().map(|node| &mut **node)
+             }
+         };
+
+         self.tail = new_tail;
+     }
+
+     pub fn pop(&mut self) -> Option<T> {
+         self.head.take().map(|head| {
+             let head = *head;
+             self.head = head.next;
+
+             if self.head.is_none() {
+                 self.tail = None;
+             }
+
+             head.elem
+         })
+     }
+}
