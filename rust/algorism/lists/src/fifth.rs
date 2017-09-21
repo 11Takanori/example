4fb702f7e54eaeca0a95fc270d29c4cfccb37d5b
Takanori Ishibashi <takanori.1112@gmail.com>
2017-09-22 08:00:52 +0900

    Add peek

--- rust/algorism/lists/src/fifth.rs
+++ rust/algorism/lists/src/fifth.rs
@@ -59,6 +59,18 @@ impl<T> List<T> {
             head.elem
         })
     }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| {
+            &node.elem
+        })
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| {
+            &mut node.elem
+        })
+    }
 }
 
 #[cfg(test)]
