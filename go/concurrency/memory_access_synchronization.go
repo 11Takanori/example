3f61ae5009eca399c95dbcdf969f9f5823620c3d
Takanori Ishibashi <takanori.1112@gmail.com>
2017-09-26 07:22:29 +0900

    Add mutex

---
+++ go/concurrency/memory_access_synchronization.go
@@ -0,0 +1,23 @@
+package main
+
+import "sync"
+import "fmt"
+
+func main() {
+	var memoryAccess sync.Mutex
+	var value int
+
+	go func() {
+		memoryAccess.Lock()
+		value++
+		memoryAccess.Unlock()
+	}()
+
+	memoryAccess.Lock()
+	if value == 0 {
+		fmt.Printf("the value is %v.\n", value)
+	} else {
+		fmt.Printf("the value is %v.\n", value)
+	}
+	memoryAccess.Unlock()
+}
