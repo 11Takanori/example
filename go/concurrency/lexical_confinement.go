0fd029ad1c69e961c5ce6564ee4e7ec973487ce1
Takanori Ishibashi <takanori.1112@gmail.com>
2017-10-19 07:18:16 +0900

    Add not concurrent-safe example

--- go/concurrency/lexical_confinement.go
+++ go/concurrency/lexical_confinement.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"sync"
 )
 
 func main() {
@@ -25,4 +27,22 @@ func main() {
 
 	results := chanOwner()
 	consumer(results)
+
+	printData := func(wg *sync.WaitGroup, data []byte) {
+		defer wg.Done()
+
+		var buff bytes.Buffer
+		for _, b := range data {
+			fmt.Fprintf(&buff, "%c", b)
+		}
+		fmt.Println(buff.String())
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	data := []byte("golang")
+	go printData(&wg, data[:3])
+	go printData(&wg, data[3:])
+
+	wg.Wait()
 }
