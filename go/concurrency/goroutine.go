6def8c89fc67c9aff093fc323f82867d9185911a
Takanori Ishibashi <takanori.1112@gmail.com>
2017-10-22 22:50:03 +0900

    Add sync

--- go/concurrency/goroutine.go
+++ go/concurrency/goroutine.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
@@ -24,4 +25,15 @@ func main() {
 	for {
 		fmt.Println(<-squares)
 	}
+
+	var wg sync.WaitGroup
+	salutation := "hello"
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		salutation = "welcome"
+	}()
+
+	wg.Wait()
+	fmt.Println(salutation)
 }
