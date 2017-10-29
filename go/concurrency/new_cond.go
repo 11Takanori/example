b62d45daf91cd9c9ca23707e8d76da87c5a00930
Takanori Ishibashi <takanori.1112@gmail.com>
2017-10-29 22:13:31 +0900

    Fix new cond

--- go/concurrency/new_cond.go
+++ go/concurrency/new_cond.go
@@ -1,8 +1,32 @@
-func main()  {
-	c := sync.NewCond(&sync.Mutex)
-	c.L.Lock()
-	for conditionTrue() == false {
-		c.Wait()
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	c := sync.NewCond(&sync.Mutex{})
+	queue := make([]interface{}, 0, 10)
+
+	removeFromQueue := func(delay time.Duration) {
+		time.Sleep(delay)
+		c.L.Lock()
+		queue = queue[1:]
+		fmt.Println("Remove from queue")
+		c.L.Unlock()
+		c.Signal()
 	}
-	c.L.Unlock()
+
+	for i := 0; i < 10; i++ {
+		c.L.Lock()
+		for len(queue) == 2 {
+			c.Wait()
+		}
+		fmt.Println("Adding to queue")
+		queue = append(queue, struct{}{})
+		go removeFromQueue(1 * time.Second)
+		c.L.Unlock()
+	}
 }
