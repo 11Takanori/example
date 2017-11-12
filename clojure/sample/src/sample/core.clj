fe4944c00eb7044064ec4276b5f1fb506091197f
Takanori Ishibashi <takanori.1112@gmail.com>
2017-11-13 07:19:16 +0900

    Add stack-consuing-fibo

--- clojure/sample/src/sample/core.clj
+++ clojure/sample/src/sample/core.clj
@@ -1,7 +1,15 @@
 (ns sample.core
   (:gen-class))
 
+(defn stack-consuming-fibo [n]
+  (cond
+    (= n 0) 0
+    (= n 1) 1
+    :else (+ (stack-consuming-fibo (- n 1))
+             (stack-consuming-fibo (- n 2)))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (println (stack-consuming-fibo 10)))
