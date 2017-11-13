4272c9e532c1616a758d02a9c5f4d8ef9c446ab2
Takanori Ishibashi <takanori.1112@gmail.com>
2017-11-14 07:12:02 +0900

    Add tail-fibo

--- clojure/sample/src/sample/core.clj
+++ clojure/sample/src/sample/core.clj
@@ -8,6 +8,14 @@
     :else (+ (stack-consuming-fibo (- n 1))
              (stack-consuming-fibo (- n 2)))))
 
+(defn tail-fibo [n]
+ (letfn [(fib
+          [current next n]
+          (if (zero? n)
+            current
+            (fib next (+ current next) (dec n))))]
+   (fib 0N 1N n)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
