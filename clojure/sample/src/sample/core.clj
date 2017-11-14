5a29e95c64d6312ee3957c34eb08e73db9b55697
Takanori Ishibashi <takanori.1112@gmail.com>
2017-11-15 07:13:26 +0900

    Add recur-fibo

--- clojure/sample/src/sample/core.clj
+++ clojure/sample/src/sample/core.clj
@@ -16,6 +16,14 @@
             (fib next (+ current next) (dec n))))]
    (fib 0N 1N n)))
 
+(defn recur-fibo [n]
+ (letfn [(fib
+          [current next n]
+          (if (zero? n)
+            current
+            (recur next (+ current next) (dec n))))]
+   (fib 0N 1N n)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
