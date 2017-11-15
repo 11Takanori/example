dfba9b07bbdb7ce88d27979f9ebbf9c943a37261
Takanori Ishibashi <takanori.1112@gmail.com>
2017-11-16 07:07:12 +0900

    Add lazy-seq-fibo

--- clojure/sample/src/sample/core.clj
+++ clojure/sample/src/sample/core.clj
@@ -24,6 +24,15 @@
             (recur next (+ current next) (dec n))))]
    (fib 0N 1N n)))
 
+(defn lazy-seq-fibo
+  ([] (concat [0 1] (lazy-seq-fibo 0N 1N)))
+  ([a b] (let [n (+ a b)](lazy-seq (cons n (lazy-seq-fibo b n))))))
+
+
+
+
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
