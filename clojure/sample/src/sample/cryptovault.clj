d388a90ced74aa1c5bb511ef12b13036a3bd2db9
Takanori Ishibashi <takanori.1112@gmail.com>
2017-11-20 07:50:54 +0900

    Fix cryptovault

--- clojure/sample/src/sample/cryptovault.clj
+++ clojure/sample/src/sample/cryptovault.clj
@@ -5,9 +5,48 @@
                           KeyStore$PasswordProtenction)
            (javax.crypto KeyGenerator Cipher CipherOutputStream
                          CipherInputStream)))
-(deftype Cryptovault [filename keystore password])
 
 (defprotocol Vault
   (init-vault [vault])
   (vault-output-stream [vault])
   (vault-input-stream [vault]))
+(defn vault-key [vault]
+  (let [password (.toCharArray (.password vault))]
+    (with-open [fis (FileInputStream. (.keystore vault))]
+      (-> (doto (KeyStore/getInstance "JCEKS")
+            (.load fis password))
+          (.getKey "vault-key" password)))))
+(deftype CryptoVault [filename keystore password]
+  Vault
+  (init-vault [vault]
+    (let [password (.toCharArray (.password vault))
+          key (.generateKey (KeyGenerator/getInstance "AES"))
+          keystore (doto (KeyStore/getInstance "JCEKS")
+                     (.load nil password)
+                     (.setEntry "vault-key"
+                                (KeyStore$SecretKeyEntry. key)
+                                (KeyStore$PasswordProtenction. password)))]
+      (with-open [fos (FileOutputStream. (.keystore vault))]
+        (.store keystore fos password))))
+
+  (vault-output-stream [vault]
+    (let [cipher (doto (Cipher/getInstance "AES")
+                   (.init Cipher/ENCRYPT_MODE (vault-key vault)))]
+      (CipherOutputStream. (io/output-stream (.filename vault)) cipher)))
+
+  (vault-input-stream [vault]
+    (let [cipher (doto (Cipher/getInstance "AES")
+                   (.init Cipher/DECRYPT_MODE (vault-key vault)))]
+      (CipherInputStream. (io/input-stream (.filename vault)) cipher)))
+
+  proto/IOFactory
+  (make-reader [vault]
+    (proto/make-reader (vault-input-stream vault)))
+  (make-writer [vault]
+    (proto/make-writer (vault-output-stream vault))))
+
+(extend CryptoVault
+  clojure.java.io/IOFactory
+  (assoc io/default-stream-impl
+    :make-input-stream (fn [x opts] (vault-input-stream x))
+    :make-outout-stream (fn [x opts] (vault-output-stream x))))
