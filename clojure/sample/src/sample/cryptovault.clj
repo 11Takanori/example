9e37b75aa5e8f84aa82b4ceb30e69c129eb45a54
Takanori Ishibashi <takanori.1112@gmail.com>
2017-11-19 20:04:03 +0900

    Add ns

--- clojure/sample/src/sample/cryptovault.clj
+++ clojure/sample/src/sample/cryptovault.clj
@@ -1,3 +1,10 @@
+(ns examples.cryptovault
+  (:use [examples.io :only [IOFactory make-reader make-writer]])
+  (:require [clojure.java.io :as io])
+  (:import (java.security Keystore KeyStore$SecretKeyEntry
+                          KeyStore$PasswordProtenction)
+           (javax.crypto KeyGenerator Cipher CipherOutputStream
+                         CipherInputStream)))
 (deftype Cryptovault [filename keystore password])
 
 (defprotocol Vault
