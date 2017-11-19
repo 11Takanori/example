897969a25f05f10bbd4ac6a8a60d238db1f9a62b
Takanori Ishibashi <takanori.1112@gmail.com>
2017-11-20 07:11:00 +0900

    Fix indent

--- clojure/sample/src/sample/core.clj
+++ clojure/sample/src/sample/core.clj
@@ -9,24 +9,26 @@
              (stack-consuming-fibo (- n 2)))))
 
 (defn tail-fibo [n]
- (letfn [(fib
-          [current next n]
-          (if (zero? n)
-            current
-            (fib next (+ current next) (dec n))))]
-   (fib 0N 1N n)))
+  (letfn [(fib
+           [current next n]
+           (if (zero? n)
+             current
+             (fib next (+ current next) (dec n))))]
+    (fib 0N 1N n)))
 
 (defn recur-fibo [n]
- (letfn [(fib
-          [current next n]
-          (if (zero? n)
-            current
-            (recur next (+ current next) (dec n))))]
-   (fib 0N 1N n)))
+  (letfn [(fib
+           [current next n]
+           (if (zero? n)
+             current
+             (recur next (+ current next) (dec n))))]
+    (fib 0N 1N n)))
 
 (defn lazy-seq-fibo
   ([] (concat [0 1] (lazy-seq-fibo 0N 1N)))
-  ([a b] (let [n (+ a b)](lazy-seq (cons n (lazy-seq-fibo b n))))))
+  ([a b] (let [n (+ a b)])
+    (lazy-seq
+      (cons n (lazy-seq-fibo b n)))))
 
 
 
