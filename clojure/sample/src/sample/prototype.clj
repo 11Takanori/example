7dff460324b22a7e0234b6e8f2f861f294c30ad7
Takanori Ishibashi <takanori.1112@gmail.com>
2017-12-05 07:26:43 +0900

    Add Prototype pattern

---
+++ clojure/sample/src/sample/prototype.clj
@@ -0,0 +1,17 @@
+(def registration-prototype
+  {:name          "Zed"
+   :email         "zzzed@gmail.com"
+   :date-of-birth "1970-01-01"
+   :weight        60
+   :gender        :male
+   :status        :single
+   :children      [{:gender :female}]
+   :month-salary  1000
+   :brands        ["Adidas" "GAP"]})
+
+(assoc registration-prototype
+  :name         "Mia Vallace"
+  :email        "tomato@gmail.com"
+  :weight       52
+  :gender       :female
+  :month-salary 0)
