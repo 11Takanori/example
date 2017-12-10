df0410eb9b7a5fb324a5def3b109a9dcd504e3e3
Takanori Ishibashi <takanori.1112@gmail.com>
2017-12-11 07:42:44 +0900

    Add builder pattern

--- clojure/sample/src/sample/builder.clj
+++ clojure/sample/src/sample/builder.clj
@@ -10,3 +10,26 @@
              :water    100
              :milk     10
              :cinnamon 3)
+
+;; mutable pattern
+(defprotocol IStringBuilder
+  (append [this s])
+  (to-string [this]))
+
+(deftype ClojureStringBuilder [charray ^:volatile-mutable last-pos]
+  IStringBuilder
+  (append [this s]
+    (let [cs (char-array s)]
+      (doseq [i (range (count cs))]
+        (aset charray (+ last-pos i) (aget cs i))))
+    (set! last-pos (+ last-pos (count s))))
+  (to-string [this] (apply str (take last-pos charray))))
+
+(defn new-string-builder []
+  (ClojureStringBuilder. (char-array 100) 0))
+
+(def sb (new-string-builder))
+(append sb "Toby Wong")
+(to-string sb)
+(append sb " ")
+(append sb "Toby Chung")
