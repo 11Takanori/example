3d1c210ba694ae1f08e659cfa802c5aad6d3e322
Takanori Ishibashi <takanori.1112@gmail.com>
2017-12-15 07:22:03 +0900

    Add Chain Of Responsibility

---
+++ clojure/sample/src/sample/chain_of_responsibity.clj
@@ -0,0 +1,21 @@
+(defn log-filter [message]
+  (logger/log message)
+  message)
+
+(defn stats-filter [message]
+  (stats/add-used-chars (count message))
+  message)
+
+(defn profanity-filter [message]
+  (clojure.string/replace message "fuck" "f*ck"))
+
+(defn reject-filter [message]
+  (if (.startsWith message "[A Profit NY]")
+    message))
+
+(defn chain [message]
+  (some-> message
+          reject-filter
+          log-filter
+          stats-filter
+          profanity-filter))
