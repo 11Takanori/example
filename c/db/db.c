47901cbdca084905f821325a968532957f83c3ee
Takanori Ishibashi <takanori.1112@gmail.com>
2017-12-23 21:57:15 +0900

    Add InputBuffer

--- c/db/db.c
+++ c/db/db.c
@@ -1,3 +1,21 @@
+struct InputBuffer_t {
+    char* buffer;
+    size_t buffer_length;
+    ssize_t input_lenght;
+};
+
+typedef struct InputBuffer_t InputBuffer;
+
+InputBuffer* new_input_buffer() {
+    InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
+    input_buffer->buffer = NULL;
+    input_buffer->input_lenght = 0;
+    input_buffer->input_lenght = 0;
+
+    return input_buffer;
+}
+
+
 int main(int argc, char* argv[]) {
     InputBuffer* input_buffer = new_input_buffer();
     while (true) {
