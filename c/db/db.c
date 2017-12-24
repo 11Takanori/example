6ed71c396b5819f43e844eba515d652ef2affeb2
Takanori Ishibashi <takanori.1112@gmail.com>
2017-12-25 07:11:14 +0900

    Add table

--- c/db/db.c
+++ c/db/db.c
@@ -80,6 +80,25 @@ void deserialize_row(void* source, Row* destination) {
     memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
+void* row_slot(Table* table, uint32_t row_num) {
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    void* page = table->pages[page_num];
+    if (!page) {
+        // Allocate memory only when we try to access page
+        page = table->pages[page_num] = malloc(PAGE_SIZE);
+    }
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+    return page + byte_offset;
+}
+
+Table* new_table() {
+  Table* table = malloc(sizeof(Table));
+  table->num_rows = 0;
+
+  return table;
+}
+
 MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
     if (strcmp(input_buffer->buffer, ".exit") == 0) {
       exit(EXIT_SUCCESS);
@@ -107,15 +126,33 @@ PrepareResult prepare_statement(InputBuffer* input_buffer,
   return PREPARE_UNRECOGNIZED_STATEMENT;
 }
 
-void execute_statement(Statement* statement) {
-    switch (statement->type) {
-      case (STATEMENT_INSERT):
-        printf("This is where we would do an insert.\n");
-        break;
-      case (STATEMENT_SELECT):
-        printf("This is where we would do a select.\n");
-        break;
-    }
+ExecuteResult execute_insert(Statement* statement, Table* table) {
+  if (table->num_rows >= TABLE_MAX_ROWS) {
+    return EXECUTE_TABLE_FULL;
+  }
+
+  Row* row_to_insert = &(statement->row_to_insert);
+  table->num_rows += 1;
+
+  return EXECUTE_SUCCESS;
+}
+
+ExecuteResult execute_select(Statement* statement, Table* table) {
+  Row row;
+  for (uint32_t i = 0; i < table->num_rows; i++) {
+    deserialize_row(row_slot(table, i), &row);
+    print_row(&row);
+  }
+  return EXIT_SUCCESS;
+}
+
+ExecuteResult execute_statement(Statement* statement, Table* table) {
+  switch(statement->type) {
+    case (STATEMENT_INSERT):
+      return execute_insert(statement, table);
+    case (STATEMENT_SELECT):
+      return execute_select(statement, table);
+  }
 }
 
 void print_prompt() { printf("db > "); }
@@ -134,6 +171,7 @@ void read_input(InputBuffer* input_buffer) {
 }
 
 int main(int argc, char* argv[]) {
+    Table* table = new_table();
     InputBuffer* input_buffer = new_input_buffer();
     while (true) {
       print_prompt();
@@ -153,13 +191,22 @@ int main(int argc, char* argv[]) {
       switch (prepare_statement(input_buffer, &statement)) {
         case (PREPARE_SUCCESS):
           break;
+        case (PREPARE_SYNTAX_ERROR):
+            printf("Syntax error. Could not parse statement.\n");
+            continue;
         case (PREPARE_UNRECOGNIZED_STATEMENT):
           printf("Unrefognized keyword at start of '%s' .\n",
                  input_buffer->buffer);
           continue;
       }
 
-      execute_statement(&statement);
-      printf("Executed.\n");
+      switch (execute_statement(&statement, table)) {
+        case (EXECUTE_SUCCESS):
+          printf("Executed.\n");
+          break;
+        case (EXECUTE_TABLE_FULL):
+          printf("Error: Table full.\n");
+          break;
+      }
     }
 }
