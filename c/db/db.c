5237b1944c02477858506c71642c79652b1bfdf3
Takanori Ishibashi <takanori.1112@gmail.com>
2017-12-30 18:54:12 +0900

    Fix do_meta_command

--- c/db/db.c
+++ c/db/db.c
@@ -176,8 +176,9 @@ InputBuffer* new_input_buffer() {
   return input_buffer;
 }
 
-MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table) {
   if (strcmp(input_buffer->buffer, ".exit") == 0) {
+    db_close(table);
     exit(EXIT_SUCCESS);
   } else {
     return META_COMMAND_UNRECOGNIZED_COMMAND;
