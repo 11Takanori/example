5553aed0fa2df3df94b9291657e3533a639e8a01
Takanori Ishibashi <takanori.1112@gmail.com>
2018-01-02 16:27:58 +0900

    Tweak

--- c/db/db.c
+++ c/db/db.c
@@ -120,11 +120,11 @@ const uint32_t LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - LEAF_NODE_HEADER_SIZE;
 const uint32_t LEAF_NODE_MAX_CELLS = LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
 
 uint32_t* leaf_node_num_cells(void* node) {
-  return (char *)node + LEAF_NODE_NUM_CELLS_OFFSET;
+  return node + LEAF_NODE_NUM_CELLS_OFFSET;
 }
 
 void* leaf_node_cell(void* node, uint32_t cell_num) {
-  return (char *)node + LEAF_NODE_HEADER_SIZE + cell_num * LEAF_NODE_CELL_SIZE;
+  return node + LEAF_NODE_HEADER_SIZE + cell_num * LEAF_NODE_CELL_SIZE;
 }
 
 uint32_t* leaf_node_key(void* node, uint32_t cell_num) {
@@ -379,7 +379,7 @@ void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value) {
   }
 
   *(leaf_node_num_cells(node)) += 1;
-  *(leaf_node_key(node, cursor->cell_num)) == key;
+  *(leaf_node_key(node, cursor->cell_num)) = key;
   serialize_row(value, leaf_node_value(node, cursor->cell_num));
 }
 
@@ -429,7 +429,7 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table) {
   if (strcmp(input_buffer->buffer, ".exit") == 0) {
     db_close(table);
     exit(EXIT_SUCCESS);
-  } else if (strcmp(input_buffer->buffer, ".btree") == 0){
+  } else if (strcmp(input_buffer->buffer, ".btree") == 0) {
     printf("Tree:\n");
     print_leaf_node(get_page(table->pager, 0));
     return META_COMMAND_SUCCESS;
