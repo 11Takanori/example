2c75801d9fa87fce5bcbd50a5f51c3df3ee456a8
Takanori Ishibashi <takanori.1112@gmail.com>
2018-01-03 10:05:23 +0900

    Add spec

--- c/db/spec/main_spec.rb
+++ c/db/spec/main_spec.rb
@@ -6,14 +6,14 @@ describe 'database' do
   def run_script(commands)
     row_output = nil
     IO.popen("./db test.db", "r+") do |pipe|
-    commands.each do |command|
-      pipe.puts command
-    end
+      commands.each do |command|
+        pipe.puts command
+      end
 
-    pipe.close_write
+      pipe.close_write
 
-    # Read entire output
-    row_output = pipe.gets(nil)
+      # Read entire output
+      row_output = pipe.gets(nil)
     end
     row_output.split("\n")
   end
@@ -149,5 +149,38 @@ describe 'database' do
       "Executed.",
       "db > ",
       ])
+  end
+
+  it 'allows printing out the structure of a 3-leaf-node btree' do
+    script = (1..14).map do |i|
+      "insert #{i} user#{i} person#{i}@example.com"
+    end
+    script << ".btree"
+    script << "insert 15 user15 person15@example.com"
+    script << ".exit"
+    result = run_script(script)
+
+    expect(result[14...(result.length)]).to match_array([
+      "db > Tree:",
+      "- internal (size 1)",
+      "  - leaf (size 7)",
+      "    - 1",
+      "    - 2",
+      "    - 3",
+      "    - 4",
+      "    - 5",
+      "    - 6",
+      "    - 7",
+      "- key 7",
+      "  - leaf (size 7)",
+      "    - 8",
+      "    - 9",
+      "    - 10",
+      "    - 11",
+      "    - 12",
+      "    - 13",
+      "    - 14",
+      "db > Need to implement searching an internal node",
+    ])
   end
 end
