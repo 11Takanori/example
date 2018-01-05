0abb514ee0a3e5e5a4332c093d9f0d99472b8e2a
Takanori Ishibashi <takanori.1112@gmail.com>
2018-01-06 00:10:53 +0900

    Add main

--- rust/db/src/main.rs
+++ rust/db/src/main.rs
@@ -1,3 +1,31 @@
+use std::io::{self, BufRead, Write};
+
+#[derive(Debug)]
+struct InputBuffer {
+    buffer: char,
+    buffer_length: usize,
+    input_length: usize,
+}
+
+impl InputBuffer {
+     fn new() -> InputBuffer {
+         unimplemented!();
+     }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+
+    loop {
+        print!(">> ");
+        io::stdout().flush().expect("Error flushing stdout");
+
+        let mut line = String::new();
+        stdin.lock().read_line(&mut line).expect("Error reading from stdin");
+
+        match &*line {
+            ".exit" => println!("{}", line),
+            _ => println!("{}", "hoge"),
+        }
+    }
 }
