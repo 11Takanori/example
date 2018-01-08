5068a217f859f2d287dbdc08a36001d08794aae7
Takanori Ishibashi <takanori.1112@gmail.com>
2018-01-08 19:39:08 +0900

    Add enum

--- rust/db/src/main.rs
+++ rust/db/src/main.rs
@@ -1,6 +1,18 @@
 use std::io::{self, BufRead, Write};
 use std::process;
 
+#[derive(Debug)]
+enum MetaCommandResult {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND,
+}
+
+#[derive(Debug)]
+enum PrepareResult {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT,
+}
+
 fn main() {
     let stdin = io::stdin();
 
