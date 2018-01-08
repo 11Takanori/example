9a6655fa9c625a4d1efa2ec6fcf2c4e363bed726
Takanori Ishibashi <takanori.1112@gmail.com>
2018-01-09 06:37:43 +0900

    Add SQL Compiler and Virtual Machine

--- rust/db/src/main.rs
+++ rust/db/src/main.rs
@@ -3,14 +3,57 @@ use std::process;
 
 #[derive(Debug)]
 enum MetaCommandResult {
-    META_COMMAND_SUCCESS,
-    META_COMMAND_UNRECOGNIZED_COMMAND,
+    MetaCommandSuccess,
+    MetaCommandUnrecognizedCommand,
+}
+
+impl MetaCommandResult {
+    fn do_meta_command(line: &String) -> MetaCommandResult {
+        match line.trim() {
+            ".exit" => process::exit(0x0100),
+            _ => MetaCommandResult::MetaCommandUnrecognizedCommand,
+        }
+    }
+}
+
+#[derive(Debug)]
+enum StatementType {
+    StatementInsert,
+    StatementSelect,
+}
+
+#[derive(Debug)]
+struct Statement {
+    t: StatementType
 }
 
 #[derive(Debug)]
 enum PrepareResult {
-    PREPARE_SUCCESS,
-    PREPARE_UNRECOGNIZED_STATEMENT,
+    PrepareSuccess,
+    PrepareUnrecognizedStatement,
+}
+
+impl PrepareResult {
+    fn parepare_statement(line: &String, statement: &mut Statement) -> PrepareResult {
+        match line.trim() {
+            "insert" => {
+                statement.t = StatementType::StatementInsert;
+                return PrepareResult::PrepareSuccess;
+            },
+            "select" => {
+                statement.t = StatementType::StatementSelect;
+                return PrepareResult::PrepareSuccess;
+            },
+            _ => PrepareResult::PrepareUnrecognizedStatement,
+        }
+    }
+}
+
+fn execute_statement(statement: Statement) {
+    match statement.t {
+        StatementType::StatementInsert => println!("This is where we would do an insert."),
+        StatementType::StatementSelect => println!("This is where we would do a select."),
+    }
 }
 
 fn main() {
@@ -23,9 +66,26 @@ fn main() {
         let mut line = String::new();
         stdin.lock().read_line(&mut line).expect("Error reading from stdin");
 
-        match line.trim() {
-            ".exit" => process::exit(0x0100),
-            _ => println!("Unrecognized command {}.", line.trim()),
+        if line.chars().nth(0) == Some('.') {
+            match MetaCommandResult::do_meta_command(&line) {
+                MetaCommandResult::MetaCommandSuccess => continue,
+                MetaCommandResult::MetaCommandUnrecognizedCommand => {
+                    println!("Unrecognized command {}.", line.trim());
+                    continue;
+                },
+            }
+        }
+
+        let mut statement = Statement{ t: StatementType::StatementSelect };
+        match PrepareResult::parepare_statement(&line, &mut statement) {
+            PrepareResult::PrepareSuccess => {
+                execute_statement(statement);
+                println!("Executed.");
+            },
+            PrepareResult::PrepareUnrecognizedStatement => {
+                println!("Unrecognized keyword at start of {}.", line.trim());
+                continue;
+            },
         }
     }
 }
