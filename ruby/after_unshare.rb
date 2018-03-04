c6100d83b7be68c279cbb4be62ec45dcd053fad5
Takanori Ishibashi <takanori.1112@gmail.com>
2018-03-04 14:17:13 +0900

    Add /

--- ruby/after_unshare.rb
+++ ruby/after_unshare.rb
@@ -2,8 +2,8 @@
 container_name = ARGV[0]
 raise unless container_name
 Dir.mkdir "sys/fs/cgroup/cpu/#{container_name}" rescue puts("skip")
-File.write "sys/fs/cgroup/cpu/#{container_name}/tasks", $$.to_s
-Dir.chroot "root/#{container_name}"
+File.write "/sys/fs/cgroup/cpu/#{container_name}/tasks", $$.to_s
+Dir.chroot "/root/#{container_name}"
 Dir.chdir "/"
 system "mount -t proc proc /proc"
 system "hostname #{container_name}.example.jp"
