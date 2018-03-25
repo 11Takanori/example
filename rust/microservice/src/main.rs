3843fa13239d30f0804d9fe3631a88ae1d959654
Takanori Ishibashi <takanori.1112@gmail.com>
2018-03-25 09:27:16 +0900

    Accepting POST Requests

--- rust/microservice/src/main.rs
+++ rust/microservice/src/main.rs
@@ -1,15 +1,84 @@
-extern crate hyper;
 extern crate futures;
+extern crate hyper;
+extern crate url;
 
+extern crate env_logger;
 #[macro_use]
 extern crate log;
-extern crate env_logger;
 
+#[macro_use]
+extern crate serde_json;
+
+use hyper::{Chunk, StatusCode};
+use hyper::Method::{Get, Post};
 use hyper::server::{Request, Response, Service};
+use hyper::header::{ContentLength, ContentType};
 
-use futures::future::Future;
+use futures::Stream;
+use futures::future::{Future, FutureResult};
+
+use std::collections::HashMap;
+use std::io;
+use std::error::Error;
+
 
 struct Microservice;
+
+struct NewMessage {
+    username: String,
+    message: String,
+}
+
+fn parse_form(form_chunk: Chunk) -> FutureResult<NewMessage, hyper::Error> {
+    let mut form = url::form_urlencoded::parse(form_chunk.as_ref())
+        .into_owned()
+        .collect::<HashMap<String, String>>();
+
+    if let Some(message) = form.remove("message") {
+        let username = form.remove("username").unwrap_or(String::from("anonymous"));
+        futures::future::ok(NewMessage {
+            username: username,
+            message: message,
+        })
+    } else {
+        futures::future::err(hyper::Error::from(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "Missing field message",
+        )))
+    }
+}
+
+fn write_to_db(entry: NewMessage) -> FutureResult<i64, hyper::Error> {
+    futures::future::ok(0)
+}
+
+fn make_post_response(
+    result: Result<i64, hyper::Error>,
+) -> FutureResult<hyper::Response, hyper::Error> {
+    match result {
+        Ok(timestamp) => {
+            let payload = json!({"timestamp": timestamp}).to_string();
+            let response = Response::new()
+                .with_header(ContentLength(payload.len() as u64))
+                .with_header(ContentType::json())
+                .with_body(payload);
+            debug!("{:?}", response);
+            futures::future::ok(response)
+        }
+        Err(error) => make_error_response(error.description()),
+    }
+}
+
+fn make_error_response(error_message: &str) -> FutureResult<hyper::Response, hyper::Error> {
+    let payload = json!({"error": error_message}).to_string();
+    let response = Response::new()
+        .with_status(StatusCode::InternalServerError)
+        .with_header(ContentLength(payload.len() as u64))
+        .with_header(ContentType::json())
+        .with_body(payload);
+    debug!("{:?}", response);
+    futures::future::ok(response)
+}
 
 impl Service for Microservice {
     type Request = Request;
@@ -18,10 +87,22 @@ impl Service for Microservice {
     type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
 
     fn call(&self, req: Self::Request) -> Self::Future {
-        info!("Microservice received a request: {:?}", req);
-        Box::new(futures::future::ok(Response::new()))
+        match (req.method(), req.path()){
+            (&Post, "/") => {
+                let future = req
+                    .body()
+                    .concat2()
+                    .and_then(parse_form)
+                    .and_then(write_to_db)
+                    .then(make_post_response);
+                Box::new(future)
+            }
+            _ => Box::new(futures::future::ok(
+                Response::new().with_status(StatusCode::NotFound)
+            ))
+        }
     }
- }
+}
 
 fn main() {
     env_logger::init();
