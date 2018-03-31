492b93a0b022301e29d266eade9a20cd1c497657
Takanori Ishibashi <takanori.1112@gmail.com>
2018-03-31 09:24:09 +0900

    Use File

--- rust/cook/src/main.rs
+++ rust/cook/src/main.rs
@@ -1,3 +1,23 @@
+use std::fs::File;
+use std::io::{Write, BufReader, BufRead};
+use std::error::Error;
+
 fn main() {
-    println!("Hello, world!");
+    run().unwrap();
 }
+
+fn run() -> Result<(), Box<Error>> {
+    let path = "lines.txt";
+    let mut output = File::create(path)?;
+    write!(output, "Rust\n💖\nFun")?;
+    let input = File::open(path)?;
+    let buffered = BufReader::new(input);
+    for line in buffered.lines() {
+        println!("{}", line?);
+    }
+
+    Ok(())
+}
+
+
+
